c022c6d099b4e62c8f8971974547a4c3c3f643a5
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-02-26 22:47:53 +0000

    modified active_scaffold_controller_for to return both forms of the controller: the constant and the path form.
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@40 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/helpers/active_scaffold_helpers.rb
+++ lib/helpers/active_scaffold_helpers.rb
@@ -20,7 +20,7 @@ module ActionView::Helpers
     ## TODO We should check the the model being used is the same Class
     ##      ie make sure ProductsController doesn't active_scaffold :shoe
     def active_scaffold_config_for(klass)
-      controller = active_scaffold_controller_for(klass)
+      controller, controller_path = active_scaffold_controller_for(klass)
       return controller.active_scaffold_config unless controller.nil? or !controller.uses_active_scaffold?
 
       config = ActiveScaffold::Config::Core.new(klass)
@@ -30,15 +30,17 @@ module ActionView::Helpers
 
     # :parent_controller, pass in something like, params[:controller], this will resolve the controller to the proper path for subsequent call to render :active_scaffold or render :component.
     def active_scaffold_controller_for(klass, parent_controller = nil)
+  		controller_path = ""
   		controller_named_path = ""
   		if parent_controller
   			path = parent_controller.split('/')
   			path.pop # remove the parent controller
   			controller_named_path = path.collect{|p| p.capitalize}.join("::") + "::"
+    		controller_path = path.join("/") + "/"
   		end
       ["#{klass.to_s}", "#{klass.to_s.pluralize}"].each do |controller_name|
         controller = "#{controller_named_path}#{controller_name.camelize}Controller".constantize rescue next
-        return controller
+        return controller, "#{controller_path}#{controller_name}"
       end
       nil
     end
