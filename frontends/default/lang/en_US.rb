4c561d0b46f07ca25ae7bc97f2e2e71c9b180997
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-03-13 19:45:20 +0000

    adding descriptions to entries in english language file
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@139 561dde7e-7729-0410-be8e-ef83869d6c7d

--- frontends/default/lang/en_US.rb
+++ frontends/default/lang/en_US.rb
@@ -1,19 +1,52 @@
 Localization.define('en_US') do |lang|
+  # Used to confirm an action. Presently used for Delete.
   lang['ARE_YOU_SURE'] ||= 'Are you sure?'
+
+  # For a cancel button
   lang['CANCEL'] ||= 'Cancel'
+
+  # For the submit button on Create forms
   lang['CREATE'] ||= 'Create'
+
+  # For flash[:info] feedback after creating a new record (in HTML mode)
   lang['CREATED %s'] ||= 'Created %s'
+
+  # The link to the Create form
   lang['CREATE_NEW'] ||= 'Create New'
+
+  # The link to delete a record
   lang['DELETE'] ||= 'Delete'
-  lang['DELETED %s'] ||= 'Deleted %s' 
+
+  # For flash[:info] feedback after deleting a record (in HTML mode)
+  lang['DELETED %s'] ||= 'Deleted %s'
+
+  # The link to the Update form
   lang['EDIT'] ||= 'Edit'
+
+  # Used for nested scaffolds to modify the scaffold title (e.g. Contacts for Client X)
   lang['FOR'] ||= ' for '
-  lang['NEXT'] ||= 'Next'
+
+  # Appears as a placeholder when there are no records on the page.
   lang['NO_ENTRIES'] ||= 'No Entries'
+
+  # For pagination - navigate to the previous page
   lang['PREVIOUS'] ||= 'Previous'
+
+  # For pagination - navigate to the next page
+  lang['NEXT'] ||= 'Next'
+
+  # For a reset button
   lang['RESET'] ||= 'Reset'
+
+  # The link to the Search interface
   lang['SEARCH'] ||= 'Search'
+
+  # The link to the Show view
   lang['SHOW'] ||= 'Show'
+
+  # For the submit button on Update forms
   lang['UPDATE'] ||= 'Update'
+
+  # For flash[:info] feedback after updating a record (in HTML mode)
   lang['UPDATED %s'] ||= 'Updated %s'
 end
