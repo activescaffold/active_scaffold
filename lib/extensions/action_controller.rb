ea1262b5d11e60068d83847af590c68ef74ca424
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-03-22 18:55:09 +0000

    bugfix to make our adapter rendering code compatible with the render's "deprecated status" argument. basically, just need to ignore it. this was being triggered by the textmate footnotes plugin.
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@262 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/extensions/action_controller.rb
+++ lib/extensions/action_controller.rb
@@ -6,7 +6,7 @@ module ActionController #:nodoc:
         @rendering_adapter = true # recursion control
         # if we need an adapter, then we render the actual stuff to a string and insert it into the adapter template
         render :file => rewrite_template_path_for_active_scaffold(params[:adapter]),
-               :locals => {:payload => render_to_string(*args, &block)},
+               :locals => {:payload => render_to_string(args.first, &block)},
                :use_full_path => true
         @rendering_adapter = nil # recursion control
       else
