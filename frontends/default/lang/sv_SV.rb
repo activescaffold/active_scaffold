546404afb43f648d2bc61b4eb5e66f00a2f5088d
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-03-27 23:58:25 +0000

    Added sv_SV
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@288 561dde7e-7729-0410-be8e-ef83869d6c7d

---
+++ frontends/default/lang/sv_SV.rb
@@ -0,0 +1,79 @@
+Localization.define('sv_SV') do |lang|
+  # Used to confirm an action. Presently used for Delete.
+  lang['ARE_YOU_SURE'] ||= '&Auml;r du s&auml;ker?'
+
+  # For a cancel button
+  lang['CANCEL_BUTTON'] ||= 'Avbryt'
+
+  # For the submit button on Create forms
+  lang['CREATE_BUTTON'] ||= 'Skapa'
+
+  # For the header label on Create forms
+  lang['CREATE_HEADER'] ||= 'Skapa'
+
+  # For the subform link on Create forms
+  lang['CREATE_ANOTHER'] ||= 'Skapa en till'
+
+  # For flash[:info] feedback after creating a new record (in HTML mode)
+  lang['CREATED %s'] ||= 'Skapad %s'
+
+  # The link to the Create From Existing form
+  lang['CREATE_FROM_EXISTING'] ||= 'L&auml;gg till fr&aring;n befintlig'
+
+  # The link to the Create form
+  lang['CREATE_NEW'] ||= 'Skapa ny'
+
+  # The link to delete a record
+  lang['DELETE_BUTTON'] ||= 'Radera'
+
+  # For flash[:info] feedback after deleting a record (in HTML mode)
+  lang['DELETED %s'] ||= 'Raderad %s'
+
+  # The link to the Update form
+  lang['EDIT_BUTTON'] ||= 'Editera'
+
+  # Used for nested scaffolds to modify the scaffold title (e.g. Contacts for Client X)
+  lang['FOR'] ||= ' f&ouml;r '
+
+  # Used in list footer, number of rows found
+  lang['FOUND'] ||= 'Hittade'
+
+  # Appears as a placeholder when there are no records on the page.
+  lang['NO_ENTRIES'] ||= 'Inga poster'
+
+  # For pagination - navigate to the previous page
+  lang['PREVIOUS_BUTTON'] ||= 'F&ouml;reg&aring;ende'
+
+  # For pagination - navigate to the next page
+  lang['NEXT_BUTTON'] ||= 'N&auml;sta'
+
+  # For the subform link on replace with new
+  lang['REMOVE_BUTTON'] ||= 'Ta bort'
+
+  # For the subform link on replace with new
+  lang['REPLACE_WITH_NEW'] ||= 'Ers&auml;tt med ny'
+
+  # For a reset button
+  lang['RESET_BUTTON'] ||= 'Nollst&auml;ll'
+
+  # The link to the Search interface
+  lang['SEARCH_BUTTON'] ||= 'S&ouml;k'
+
+  # Used in select controls to entice user to select something
+  lang['_SELECT_'] ||= '- v&auml;lj -'
+
+  # The link to the Show view
+  lang['SHOW_BUTTON'] ||= 'Visa'
+
+  # The header label on the Show view
+  lang['SHOW_HEADER'] ||= 'Visa'
+
+  # For the submit button on Update forms
+  lang['UPDATE_BUTTON'] ||= 'Uppdatera'
+
+  # For the header label on Update forms
+  lang['UPDATE_HEADER'] ||= 'Uppdatere'
+
+  # For flash[:info] feedback after updating a record (in HTML mode)
+  lang['UPDATED %s'] ||= 'Uppdaterad %s'
+end
