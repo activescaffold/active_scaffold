322f4cb7700dfa14955dce31b6660be17fa93bd3
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-04-02 16:21:16 +0000

    Change Localization methods so that they play nice with localization plugins such as Globalize.
    Added nb-no language
    
    Removed static "close" in inline_adapter
    
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@304 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/localization.rb
+++ lib/localization.rb
@@ -9,20 +9,15 @@ module ActiveScaffold
     @@lang = 'en-us'
 
     def self._(string_to_localize, *args)
+      sprintf translate(string_to_localize, *args)
+    end
+
+    def self.translate(string_to_localize, *args)
       if @@l10s[@@lang].nil? or @@l10s[@@lang][string_to_localize].nil?
-        translated = string_to_localize
+        string_to_localize
       else
-        translated = @@l10s[@@lang][string_to_localize]
+        @@l10s[@@lang][string_to_localize]
       end
-      return translated.call(*args).to_s  if translated.is_a? Proc
-      if translated.is_a? Array
-        translated = if translated.size == 3
-          translated[args[0]==0 ? 0 : (args[0]>1 ? 2 : 1)]
-        else
-          translated[args[0]>1 ? 1 : 0]
-        end
-      end
-      sprintf translated, *args
     end
 
     def self.define(lang = 'en-us')
