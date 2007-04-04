401a7681e83d400c4a2b3882428558d41f4b18af
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-04-04 16:19:54 +0000

    patching in support for options[:name] in date/time/datetime helpers
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@314 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/extensions/action_view.rb
+++ lib/extensions/action_view.rb
@@ -74,10 +74,10 @@ module ActionView #:nodoc:
   end
 end
 
-# patch an issue with integer size parameters
 module ActionView
   module Helpers
     class InstanceTag
+      # patch an issue with integer size parameters
       def to_text_area_tag(options = {})
         options = DEFAULT_TEXT_AREA_OPTIONS.merge(options.stringify_keys)
         add_default_name_and_id(options)
@@ -92,6 +92,18 @@ module ActionView
           content_tag("textarea", html_escape(options.delete('value') || value_before_type_cast), options)
         end
       end
+
+      private
+
+      # patch in support for options[:name]
+      def options_with_prefix_with_name(position, options)
+        if options[:name]
+          options.merge(:prefix => options[:name].dup.insert(-2, "(#{position}i)"))
+        else
+          options_with_prefix_without_name
+        end
+      end
+      alias_method_chain :options_with_prefix, :name
     end
   end
 end
