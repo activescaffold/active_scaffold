b6dedcf6b8095fc22dee73714925f852d9f1a123
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-04-09 15:46:55 +0000

    fixing bug in the options_with_prefix_with_name extension for action_view. it wasn't behaving well if there was *no* options[:name].
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@332 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/extensions/action_view.rb
+++ lib/extensions/action_view.rb
@@ -100,7 +100,7 @@ module ActionView
         if options[:name]
           options.merge(:prefix => options[:name].dup.insert(-2, "(#{position}i)"))
         else
-          options_with_prefix_without_name
+          options_with_prefix_without_name(position, options)
         end
       end
       alias_method_chain :options_with_prefix, :name
