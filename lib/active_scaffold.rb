6a785e3299153538429bbdd281b882571c6850d5
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-05-22 19:23:35 +0000

    fixed a data handling problem: as_() would run sprintf on dynamic data that could contain bad %-codes. now, it will only run sprintf if it receives extra args, and in all those situations the format string is static.
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@489 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/active_scaffold.rb
+++ lib/active_scaffold.rb
@@ -120,6 +120,6 @@ end
 
 class Object
   def as_(string_to_localize, *args)
-    sprintf string_to_localize, *args
+    args.empty? ? string_to_localize : (sprintf string_to_localize, *args)
   end
 end
