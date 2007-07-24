3797397f1ce8fb1d39e090357813ed70f74502af
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-07-24 19:43:29 +0000

    RESTful routing is now accomplished in a way that'll let other add-ons extend the meaning of :active_scaffold => true.
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@583 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/extensions/resources.rb
+++ lib/extensions/resources.rb
@@ -1,14 +1,19 @@
 module ActionController
   module Resources
     class Resource
+      ACTIVE_SCAFFOLD_ROUTING = {
+        :collection => {:show_search => :get, :update_table => :get, :edit_associated => :get, :list => :get, :new_existing => :get},
+        :member => {:row => :get, :nested => :get, :edit_associated => :get, :add_association => :get}
+      }
+
       # by overwriting the attr_reader :options, we can parse out a special :active_scaffold flag just-in-time.
       def options
         if @options.delete :active_scaffold
           logger.info "ActiveScaffold: extending RESTful routes for #{@plural}"
           @options[:collection] ||= {}
-          @options[:collection].merge! :show_search => :get, :update_table => :get, :edit_associated => :get, :list => :get, :new_existing => :get
+          @options[:collection].merge! ACTIVE_SCAFFOLD_ROUTING[:collection]
           @options[:member] ||= {}
-          @options[:member].merge! :row => :get, :nested => :get, :edit_associated => :get, :add_association => :get
+          @options[:member].merge! ACTIVE_SCAFFOLD_ROUTING[:member]
         end
         @options
       end
