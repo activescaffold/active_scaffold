244cd5fd89a14363fe78e1ce92cf8d23820236c7
cainlevy <cainlevy@561dde7e-7729-0410-be8e-ef83869d6c7d>
2007-11-03 18:06:34 +0000

    adding :update_column => :post to the RESTful routing. closes issue #448.
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@662 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/extensions/resources.rb
+++ lib/extensions/resources.rb
@@ -3,7 +3,7 @@ module ActionController
     class Resource
       ACTIVE_SCAFFOLD_ROUTING = {
         :collection => {:show_search => :get, :update_table => :get, :edit_associated => :get, :list => :get, :new_existing => :get},
-        :member => {:row => :get, :nested => :get, :edit_associated => :get, :add_association => :get}
+        :member => {:row => :get, :nested => :get, :edit_associated => :get, :add_association => :get, :update_column => :post}
       }
 
       # by overwriting the attr_reader :options, we can parse out a special :active_scaffold flag just-in-time.
