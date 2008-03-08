472f925a283d11c4a592859e0a78d39974b2d23e
edwin.moss <edwin.moss@561dde7e-7729-0410-be8e-ef83869d6c7d>
2008-03-08 03:06:02 +0000

    How did that left_handed get in there?
    git-svn-id: http://activescaffold.googlecode.com/svn/trunk@733 561dde7e-7729-0410-be8e-ef83869d6c7d

--- lib/helpers/view_helpers.rb
+++ lib/helpers/view_helpers.rb
@@ -92,9 +92,7 @@ module ActiveScaffold
         js = javascript_include_tag(ActiveScaffold::Config::Core.asset_path('input_box.js', frontend))
         js << javascript_include_tag(ActiveScaffold::Config::Core.asset_path('number_formats.js', frontend))
         css = stylesheet_link_tag(ActiveScaffold::Config::Core.asset_path('tools-stylesheet.css', frontend))
-        css << stylesheet_link_tag(ActiveScaffold::Config::Core.asset_path('tools-left-handed-stylesheet.css', frontend)) if ActiveScaffold::Config::Core.left_handed
         ie_css = stylesheet_link_tag(ActiveScaffold::Config::Core.asset_path('tools-stylesheet-ie.css', frontend))
-        ie_css << stylesheet_link_tag(ActiveScaffold::Config::Core.asset_path('tools-left-handed-stylesheet-ie.css', frontend)) if ActiveScaffold::Config::Core.left_handed
         active_scaffold_includes_without_tools + "\n" + js + "\n" + css + "\n<!--[if IE]>" + ie_css + "<![endif]-->\n"
       end
       alias_method_chain :active_scaffold_includes, :tools
