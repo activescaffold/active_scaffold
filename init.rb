2515d0e968ed32d47d8a127f079ba333cf0c934c
Tim Harper <timcharper@gmail.com>
2008-06-29 02:07:21 -0600

    raise if Rails is not 2.0 or 1.2.x

--- init.rb
+++ init.rb
@@ -1,6 +1,8 @@
 ##
 ## Initialize the environment
 ##
+raise "This version of ActiveScaffold is compatible with Rails 2.0 or Rails 1.2.x.  Please use a later version." unless (Rails::VERSION::MAJOR == 2 && Rails::VERSION::MINOR == 0) || (Rails::VERSION::MAJOR == 1 && Rails::VERSION::MINOR >= 2)
+
 require File.dirname(__FILE__) + '/environment'
 
 ##
