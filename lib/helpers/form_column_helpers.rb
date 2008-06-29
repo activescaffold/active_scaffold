activescaffold/active_scaffold

commit 18404cf1b94a39fde09bfdb579949bd166a2c78d

record_select gives a helpful error if specifying the wrong type of column

lib/helpers/form_column_helpers.rb
@@ -109,6 +109,7 @@ module ActiveScaffold
       # requires RecordSelect plugin to be installed and configured.
       # ... maybe this should be provided in a bridge?
       def active_scaffold_input_record_select(column, options)
+        raise ArgumentError, "record_select can only work against associations (and #{column.name} is not).  A common mistake is to specify the foreign key field (like :user_id), instead of the association (:user)."
         remote_controller = active_scaffold_controller_for(column.association.klass).controller_path
 
         # if the opposite association is a :belongs_to, then only show records that have not been associated yet

commit 2e37b3329b5f85c213729dd6d24e3b82d6032b2f

Fix recordselect columns which were broken by commit 18404cf1b94a39fde09bfdb579949bd166a2c78d

lib/helpers/form_column_helpers.rb
@@ -109,7 +109,9 @@ module ActiveScaffold
       # requires RecordSelect plugin to be installed and configured.
       # ... maybe this should be provided in a bridge?
       def active_scaffold_input_record_select(column, options)
-        raise ArgumentError, "record_select can only work against associations (and #{column.name} is not).  A common mistake is to specify the foreign key field (like :user_id), instead of the association (:user)."
+        unless column.association
+          raise ArgumentError, "record_select can only work against associations (and #{column.name} is not).  A common mistake is to specify the foreign key field (like :user_id), instead of the association (:user)."
+        end
         remote_controller = active_scaffold_controller_for(column.association.klass).controller_path
 
         # if the opposite association is a :belongs_to, then only show records that have not been associated yet