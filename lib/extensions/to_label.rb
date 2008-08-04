dad960764f26eb41a2092d0530f1637de21374a2
Lance Ivy <lance@cainlevy.net>
2008-08-04 08:05:48 -0700

    to_label now uses :id field as last resort. closes issue 597.

--- lib/extensions/to_label.rb
+++ lib/extensions/to_label.rb
@@ -1,7 +1,7 @@
 # the ever-useful to_label method
 class ActiveRecord::Base
   def to_label
-    [:name, :label, :title, :to_s].each do |attribute|
+    [:name, :label, :title, :to_s, :id].each do |attribute|
       return send(attribute) if respond_to?(attribute) and send(attribute).is_a?(String)
     end
   end
