40268fb4df803fe1b213db04b8e18d854b7d0725
Lance Ivy <lance@cainlevy.net>
2008-08-04 11:29:23 -0700

    ok, to_label doesn't return an id, instead it returns to_s on the first responding attribute.

--- lib/extensions/to_label.rb
+++ lib/extensions/to_label.rb
@@ -1,8 +1,8 @@
 # the ever-useful to_label method
 class ActiveRecord::Base
   def to_label
-    [:name, :label, :title, :to_s, :id].each do |attribute|
-      return send(attribute) if respond_to?(attribute) and send(attribute).is_a?(String)
+    [:name, :label, :title, :to_s].each do |attribute|
+      return send(attribute).to_s if respond_to?(attribute)
     end
   end
 end
