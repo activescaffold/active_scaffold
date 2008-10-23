c7b11a66f18859bb477caa801893a73afafa069d
Sergio Cambra <sergio@entrecables.com>
2008-10-23 20:21:06 +0200

    add render action list without layout for javascript format

--- lib/actions/list.rb
+++ lib/actions/list.rb
@@ -36,6 +36,7 @@ module ActiveScaffold::Actions
         type.html {
           render :action => 'list', :layout => true
         }
+        type.js { render :action => 'list', :layout => false }
         type.xml { render :xml => response_object.to_xml, :content_type => Mime::XML, :status => response_status }
         type.json { render :text => response_object.to_json, :content_type => Mime::JSON, :status => response_status }
         type.yaml { render :text => response_object.to_yaml, :content_type => Mime::YAML, :status => response_status }
