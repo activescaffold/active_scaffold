activescaffold/active_scaffold

commit 0ef10210eee7918fdb2f941dfaee02caf97284e9

Fix #404 http://code.google.com/p/activescaffold/issues/detail?id=404 Change active_scaffold_input_singular_association in order to rails marks the field with error. Some cleanup of that method.

lib/helpers/form_column_helpers.rb
@@ -60,15 +60,11 @@ module ActiveScaffold
       def active_scaffold_input_singular_association(column, options)
         associated = @record.send(column.association.name)
 
-        options_for_select = options_for_association(column.association)
-        select_options = [[as_('- select -'),nil]]
-        select_options += [[ associated.to_label, associated.id ]] unless associated.nil? or options_for_select.find {|label, id| id == associated.id}
-        select_options += options_for_select
-
-        selected = associated.nil? ? nil : associated.id
+        select_options = options_for_association(column.association)
+        select_options.unshift([ associated.to_label, associated.id ]) unless associated.nil? or select_options.find {|label, id| id == associated.id}
 
         options[:name] += '[id]'
-        select(:record, column.name, select_options.uniq, { :selected => selected }, options)
+        select(:record, "#{column.name}_id", select_options.uniq, {:include_blank => as_('- select -')}, options)
       end
 
       def active_scaffold_input_plural_association(column, options)

commit d56ac332b3963cc71042a4d127e8f22b398d33bc

Fix form_ui form_ui :select for has_one associations, was broken by commit 0ef1021

lib/helpers/form_column_helpers.rb
@@ -63,8 +63,10 @@ module ActiveScaffold
         select_options = options_for_association(column.association)
         select_options.unshift([ associated.to_label, associated.id ]) unless associated.nil? or select_options.find {|label, id| id == associated.id}
 
+        selected = associated.nil? ? nil : associated.id
+        method = column.association.macro == :belongs_to ? column.association.primary_key_name : column.name
         options[:name] += '[id]'
-        select(:record, "#{column.name}_id", select_options.uniq, {:include_blank => as_('- select -')}, options)
+        select(:record, method, select_options.uniq, {:selected => selected, :include_blank => as_('- select -')}, options)
       end
 
       def active_scaffold_input_plural_association(column, options)