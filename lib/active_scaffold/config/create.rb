59ffc637daecfb5468d719492c66294cfff39a1a
Mike Gaffney <mike.gaffney@asolutions.com>
2009-01-02 12:28:18 -0600

    This feature wasn't fully implemented. Kenny asked that this be backed out. Also fixes http://code.google.com/p/activescaffold/issues/detail?id=642
    show_on_create will be moving to list config.
    
    Revert "added show_on_list for create and backfilled tests for create"
    
    This reverts commit 639f37972e04bb58390bc51853bed16459bb43bd.

--- lib/active_scaffold/config/create.rb
+++ lib/active_scaffold/config/create.rb
@@ -4,7 +4,6 @@ module ActiveScaffold::Config
     def initialize(*args)
       super
       self.persistent = self.class.persistent
-      self.show_on_list = self.class.show_on_list
       self.edit_after_create = self.class.edit_after_create
     end
 
@@ -19,10 +18,6 @@ module ActiveScaffold::Config
     end
     @@link = ActiveScaffold::DataStructures::ActionLink.new('new', :label => 'Create New', :type => :table, :security_method => :create_authorized?)
 
-    # whether the create form is initially displayed on list or not
-    cattr_accessor :show_on_list
-    @@show_on_list = false
-    
     # whether the form stays open after a create or not
     cattr_accessor :persistent
     @@persistent = false
@@ -38,9 +33,6 @@ module ActiveScaffold::Config
       @label ? as_(@label) : as_('Create %s', @core.label.singularize)
     end
 
-    # whether the create form is initially displayed on list or not
-    attr_accessor :show_on_list
-    
     # whether the form stays open after a create or not
     attr_accessor :persistent
 
