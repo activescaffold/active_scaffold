171d406b229b1ffe80d8aa9640be896f8356c499
Kenny Ortmann <kenny.ortmann@gmail.com>
2009-03-13 08:29:02 -0500

    cleaned up date select more

--- lib/extensions/name_option_for_datetime.rb
+++ lib/extensions/name_option_for_datetime.rb
@@ -2,20 +2,9 @@ module ActionView
   module Helpers
     class InstanceTag
       private
-      # patch in support for options[:name]
       def datetime_selector_with_name(options, html_options)
-        datetime = value(object) || default_datetime(options)
-
-        options = options.dup
-        options[:field_name]           = @method_name
-        options[:include_position]     = true
-        options[:prefix]             ||= @object_name
-        options[:index]                = @auto_index if @auto_index && !options.has_key?(:index)
-        options[:datetime_separator] ||= ' &mdash; '
-        options[:time_separator]     ||= ' : '
         options.merge!(:prefix => options[:name].gsub(/\[[^\[]*\]$/,'')) if options[:name]
-
-        DateTimeSelector.new(datetime, options.merge(:tag => true), html_options)
+        datetime_selector_without_name(options, html_options)        
       end
       alias_method_chain :datetime_selector, :name
     end
