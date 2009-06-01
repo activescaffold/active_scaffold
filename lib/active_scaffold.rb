634a21c6ff6c4e81d1a10dcb74e8209389cca81d
Sergio <sergio@entrecables.com>
2009-06-01 09:39:31 +0200

    We don't want to mask errors

--- lib/active_scaffold.rb
+++ lib/active_scaffold.rb
@@ -85,7 +85,7 @@ module ActiveScaffold
         include ActiveScaffold::Actions::Core
         active_scaffold_config.actions.each do |mod|
           name = mod.to_s.camelize
-          include "ActiveScaffold::Actions::#{name}".constantize rescue nil
+          include "ActiveScaffold::Actions::#{name}".constantize
 
           # sneak the action links from the actions into the main set
           if link = active_scaffold_config.send(mod).link rescue nil
