9e4bd651e3bdf2f49d76b2d96fce55deecfb24da
Kenny Ortmann <kenny.ortmann@gmail.com>
2009-08-05 11:10:12 -0500

    adding in try statement for when page.items returns nil

--- lib/active_scaffold/actions/list.rb
+++ lib/active_scaffold/actions/list.rb
@@ -70,7 +70,7 @@ module ActiveScaffold::Actions
       end
 
       page = find_page(options);
-      if page.items.empty?
+      if page.items.try(:empty?)
         page = page.pager.first
         active_scaffold_config.list.user.page = 1
       end
