8e1b72202fd54064fee3142d7bf8a1fc45833649
Volker Hochstein <v.hochstein@highstone.de>
2009-08-14 15:31:44 +0200

    get tests at least up and running, however 3 tests are failing

--- test/misc/finder_test.rb
+++ test/misc/finder_test.rb
@@ -24,7 +24,7 @@ class FinderTest < Test::Unit::TestCase
 			'(LOWER(model_stubs.a) LIKE ? OR LOWER(model_stubs.b) LIKE ?) AND (LOWER(model_stubs.a) LIKE ? OR LOWER(model_stubs.b) LIKE ?)',
 		  '%foo%', '%foo%', '%bar%', '%bar%'
 		]
-    assert_equal expected_conditions, ActiveScaffold::Finder.create_conditions_for_columns(tokens, columns)
+    assert_equal expected_conditions, ClassWithFinder.create_conditions_for_columns(tokens, columns)
 
     expected_conditions = [
       '(LOWER(model_stubs.a) LIKE ? OR LOWER(model_stubs.b) LIKE ?)',
