099a302f4ee2bfe98383ee8fbb80eba994891695
Sergio <sergio@entrecables.com>
2009-09-25 13:39:08 +0200

    Fix default sorting when default_scope use a symbol for order

--- lib/active_scaffold/data_structures/sorting.rb
+++ lib/active_scaffold/data_structures/sorting.rb
@@ -12,7 +12,7 @@ module ActiveScaffold::DataStructures
       if model.default_scoping.last.nil?  || model.default_scoping.last[:find].nil? || model.default_scoping.last[:find][:order].nil?
         set(model.primary_key, 'ASC') if model.column_names.include?(model.primary_key)
       else
-        set_sorting_from_order_clause(model.default_scoping.last[:find][:order])
+        set_sorting_from_order_clause(model.default_scoping.last[:find][:order].to_s)
       end
     end
     
