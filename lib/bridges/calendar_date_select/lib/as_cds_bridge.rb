8cb194f47bab7768d0a81ac9c62342b5ba2f9ff7
Sergio <sergio@entrecables.com>
2009-10-26 17:11:02 +0100

    Add search_ui for search ranges in field_search with calendar date select

--- lib/bridges/calendar_date_select/lib/as_cds_bridge.rb
+++ lib/bridges/calendar_date_select/lib/as_cds_bridge.rb
@@ -21,7 +21,7 @@ end
 
 
 module ActiveScaffold
-  module Helpers
+  module CalendarDateSelectBridge
     # Helpers that assist with the rendering of a Form Column
     module FormColumnHelpers
       def active_scaffold_input_calendar_date_select(column, options)
@@ -29,25 +29,58 @@ module ActiveScaffold
         calendar_date_select("record", column.name, options.merge(column.options))
       end      
     end
+
+    module SearchColumnHelpers
+      def active_scaffold_search_calendar_date_select(column, options)
+        options = column.options.merge(options)
+        helper = "select_#{'date' unless options[:discard_date]}#{'time' unless options[:discard_time]}"
+        html = []
+        html << calendar_date_select("record", column.name, options.merge(:name => "#{options[:name]}[from]", :id => "#{options[:id]}_from"))
+        html << calendar_date_select("record", column.name, options.merge(:name => "#{options[:name]}[to]", :id => "#{options[:id]}_to"))
+        html * ' - '
+      end
+    end
+
+    module ViewHelpers
+      # Provides stylesheets to include with +stylesheet_link_tag+
+      def active_scaffold_stylesheets(frontend = :default)
+        super + [calendar_date_select_stylesheets]
+      end
+
+      # Provides stylesheets to include with +stylesheet_link_tag+
+      def active_scaffold_javascripts(frontend = :default)
+        super + [calendar_date_select_javascripts]
+      end
+    end
+
+    module Finder
+      module ClassMethods
+        def condition_for_calendar_date_select_type(column, value, like_pattern)
+          conversion = column.column.type == :date ? 'to_date' : 'to_time'
+          from_value, to_value = ['from', 'to'].collect do |field|
+            Time.zone.parse(value[field]) rescue nil
+          end
+
+          if from_value.nil? and to_value.nil?
+            nil
+          elsif !from_value
+            ["#{column.search_sql} <= ?", to_value.send(conversion).to_s(:db)]
+          elsif !to_value
+            ["#{column.search_sql} >= ?", from_value.send(conversion).to_s(:db)]
+          else
+            ["#{column.search_sql} BETWEEN ? AND ?", from_value.send(conversion).to_s(:db), to_value.send(conversion).to_s(:db)]
+          end
+        end
+      end
+    end
   end
 end
 
-module ActiveScaffold
-  module Helpers
-    module ViewHelpers
-
-      # Provides stylesheets to include with +stylesheet_link_tag+
-      def active_scaffold_stylesheets_with_calendar_date_select(frontend = :default)
-        active_scaffold_stylesheets_without_calendar_date_select.to_a << calendar_date_select_stylesheets
-      end
-      alias_method_chain :active_scaffold_stylesheets, :calendar_date_select
-
-      # Provides stylesheets to include with +stylesheet_link_tag+
-      def active_scaffold_javascripts_with_calendar_date_select(frontend = :default)
-        active_scaffold_javascripts_without_calendar_date_select.to_a << calendar_date_select_javascripts
-      end
-      alias_method_chain :active_scaffold_javascripts, :calendar_date_select
-      
-    end
-  end
+ActionView::Base.class_eval do
+  include ActiveScaffold::CalendarDateSelectBridge::FormColumnHelpers
+  include ActiveScaffold::CalendarDateSelectBridge::SearchColumnHelpers
+  include ActiveScaffold::CalendarDateSelectBridge::ViewHelpers
+end
+ActiveScaffold::Finder::ClassMethods.module_eval do
+  include ActiveScaffold::CalendarDateSelectBridge::Finder::ClassMethods
 end
