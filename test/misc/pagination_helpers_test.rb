b0cedeb58e07736a91de98b9d92c739e13dcfb4d
scambra <sergio@entrecables.com>
2010-02-04 14:45:15 +0100

    Fix latest commit, test were wrong

--- test/misc/pagination_helpers_test.rb
+++ test/misc/pagination_helpers_test.rb
@@ -6,24 +6,24 @@ class PaginationHelpersTest < Test::Unit::TestCase
   def test_links
     self.stubs(:pagination_ajax_link).returns('l')
 
-    assert '1', links(1, 1)
-    assert '1 l', links(1, 2)
-    assert '1 l l', links(1, 3)
-    assert '1 l l l', links(1, 4)
-    assert '1 l l .. l', links(1, 5)
-    assert '1 l l .. l', links(1, 6)
+    assert_equal '1', links(1, 1)
+    assert_equal '1 l', links(1, 2)
+    assert_equal '1 l l', links(1, 3)
+    assert_equal '1 l l l', links(1, 4)
+    assert_equal '1 l l .. l', links(1, 5)
+    assert_equal '1 l l .. l', links(1, 6)
 
-    assert 'l 1 l l .. l', links(2, 10)
-    assert 'l l 1 l l .. l', links(3, 10)
-    assert 'l l l 1 l l .. l', links(4, 10)
-    assert 'l .. l l 1 l l .. l', links(5, 10)
-    assert 'l .. l l 1 l l .. l', links(6, 10)
+    assert_equal 'l 2 l l .. l', links(2, 10)
+    assert_equal 'l l 3 l l .. l', links(3, 10)
+    assert_equal 'l l l 4 l l .. l', links(4, 10)
+    assert_equal 'l .. l l 5 l l .. l', links(5, 10)
+    assert_equal 'l .. l l 6 l l .. l', links(6, 10)
 
-    assert '1 l l l l', links(1, 5, 3)
-    assert '1 l l l .. l', links(1, 6, 3)
-    assert 'l l l l 1 l l .. l', links(5, 10, 3)
-    assert 'l .. l l l 1 l l .. l', links(6, 10, 3)
-    assert 'l .. l l l 1 l l l .. l', links(6, 20, 3)
+    assert_equal '1 l l l l', links(1, 5, 3)
+    assert_equal '1 l l l .. l', links(1, 6, 3)
+    assert_equal 'l l l l 5 l l l .. l', links(5, 10, 3)
+    assert_equal 'l .. l l l 6 l l l l', links(6, 10, 3)
+    assert_equal 'l .. l l l 6 l l l .. l', links(6, 20, 3)
   end
 
   private
