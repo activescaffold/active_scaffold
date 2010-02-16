a67548b20b74052b0b9ad00d84d9074f6ed0cc1f
scambra <sergio@entrecables.com>
2010-02-16 14:24:34 +0100

    Allow to change like search, fixes #369 applying it to search and live_search too

--- lib/active_scaffold/actions/search.rb
+++ lib/active_scaffold/actions/search.rb
@@ -21,8 +21,8 @@ module ActiveScaffold::Actions
 
       unless @query.empty?
         columns = active_scaffold_config.search.columns
-        like_pattern = active_scaffold_config.search.full_text_search? ? '%?%' : '?%'
-        search_conditions = self.class.create_conditions_for_columns(@query.split(' '), columns, like_pattern)
+        text_search = active_scaffold_config.search.text_search
+        search_conditions = self.class.create_conditions_for_columns(@query.split(' '), columns, text_search)
         self.active_scaffold_conditions = merge_conditions(self.active_scaffold_conditions, search_conditions)
         @filtered = !search_conditions.blank?
 
