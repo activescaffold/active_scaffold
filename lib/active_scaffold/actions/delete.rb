25e9d3bdc4e9a7f5f611fdecb00d82e933f5827e
scambra <sergio@entrecables.com>
2010-02-16 11:01:01 +0100

    Fix response_status in json, xml and yaml formats and respect config columns, fixes #646

--- lib/active_scaffold/actions/delete.rb
+++ lib/active_scaffold/actions/delete.rb
@@ -28,15 +28,15 @@ module ActiveScaffold::Actions
     end
 
     def destroy_respond_to_xml
-      render :xml => successful? ? "" : response_object.to_xml, :content_type => Mime::XML, :status => response_status
+      render :xml => successful? ? "" : response_object.to_xml(:only => active_scaffold_config.list.columns.names), :content_type => Mime::XML, :status => response_status
     end
 
     def destroy_respond_to_json
-      render :text => successful? ? "" : response_object.to_json, :content_type => Mime::JSON, :status => response_status
+      render :text => successful? ? "" : response_object.to_json(:only => active_scaffold_config.list.columns.names), :content_type => Mime::JSON, :status => response_status
     end
 
     def destroy_respond_to_yaml
-      render :text => successful? ? "" : response_object.to_yaml, :content_type => Mime::YAML, :status => response_status
+      render :text => successful? ? "" : Hash.from_xml(response_object.to_xml(:only => active_scaffold_config.list.columns.names)).to_yaml, :content_type => Mime::YAML, :status => response_status
     end
 
     def destroy_find_record
