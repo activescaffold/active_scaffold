activescaffold/active_scaffold

commit dd2f2c80e2f77d6f8d0748ac0a9ba774143a85a4

Get rid of update_table action and respond to js in list action updating the content

lib/active_scaffold/actions/list.rb
@@ -1,7 +1,7 @@
 module ActiveScaffold::Actions
   module List
     def self.included(base)
-      base.before_filter :list_authorized_filter, :only => [:index, :table, :update_table, :row, :list]
+      base.before_filter :list_authorized_filter, :only => [:index, :table, :row, :list]
     end
 
     def index
@@ -11,12 +11,6 @@ module ActiveScaffold::Actions
     def table
       do_list
       render(:action => 'list', :layout => false)
-    end
-
-    # This is called when changing pages, sorts and search
-    def update_table
-      do_list
-      respond_to_action(:update_table)
     end
 
     # get just a single row
@@ -36,7 +30,7 @@ module ActiveScaffold::Actions
       render :action => 'list'
     end
     def list_respond_to_js
-      render :action => 'list', :layout => false
+      render :action => 'list.js'
     end
     def list_respond_to_xml
       render :xml => response_object.to_xml(:only => active_scaffold_config.list.columns.names), :content_type => Mime::XML, :status => response_status
@@ -46,12 +40,6 @@ module ActiveScaffold::Actions
     end
     def list_respond_to_yaml
       render :text => Hash.from_xml(response_object.to_xml(:only => active_scaffold_config.list.columns.names)).to_yaml, :content_type => Mime::YAML, :status => response_status
-    end
-    def update_table_respond_to_html
-      return_to_main
-    end
-    def update_table_respond_to_js
-      render(:partial => 'list')
     end
     # The actual algorithm to prepare for the list view
     def do_list
@@ -85,9 +73,6 @@ module ActiveScaffold::Actions
     private
     def list_authorized_filter
       raise ActiveScaffold::ActionNotAllowed unless list_authorized?
-    end
-    def update_table_formats
-      (default_formats + active_scaffold_config.formats).uniq
     end
     def list_formats
       (default_formats + active_scaffold_config.formats + active_scaffold_config.list.formats).uniq

frontends/default/javascripts/active_scaffold.js
@@ -48,9 +48,8 @@ var ActiveScaffold = {
     }
   },
   reload_if_empty: function(tbody, url) {
-    var content_container_id = tbody.replace('tbody', 'content');
     if (this.records_for(tbody).length == 0) {
-      new Ajax.Updater($(content_container_id), url, {
+      new Ajax.Request(url, {
         method: 'get',
         asynchronous: true,
         evalScripts: true

frontends/default/javascripts/dhtml_history.js
@@ -858,7 +858,7 @@ var handleHistoryChange = function(pageId, pageData) {
   var info = pageId.split(':');
   var id = info[0];
   pageData += '&_method=get';
-  new Ajax.Updater(id+'-content', pageData, {asynchronous:true, evalScripts:true, method: 'get', onLoading:function(request){Element.show(id+'-pagination-loading-indicator');}});
+  new Ajax.Request(pageData, {asynchronous:true, evalScripts:true, method: 'get', onLoading:function(request){Element.show(id+'-pagination-loading-indicator');}});
 }
 
 window.onload = function() {

frontends/default/views/_field_search.html.erb
@@ -2,7 +2,7 @@
 # We have to remove search form params before the url_for method call, otherwise it throughs it on
 search_params = params[:search]
 params.merge!(:search => nil)
-href = url_for(params_for(:action => :update_table, :escape => false).delete_if{|k,v| k == 'search'})
+href = url_for(params_for(:action => :index, :escape => false).delete_if{|k,v| k == 'search'})
 -%>
 <%= form_remote_tag :url => href,
                     :method => :get,
@@ -10,7 +10,6 @@ href = url_for(params_for(:action => :update_table, :escape => false).delete_if{
                     :after => "$('#{loading_indicator_id(:action => :search, :id => params[:id])}').style.visibility = 'visible'; Form.disable('#{search_form_id}');",
                     :complete => "$('#{loading_indicator_id(:action => :search, :id => params[:id])}').style.visibility = 'hidden'; Form.enable('#{search_form_id}');",
                     :failure => "ActiveScaffold.report_500_response('#{active_scaffold_id}')",
-                    :update => active_scaffold_content_id,
                     :html => { :action => href, :id => search_form_id, :class => 'search', :method => :get } %>
 
   <ol class="form">

frontends/default/views/_list_column_headings.html.erb
@@ -8,7 +8,7 @@ default_sorting_stages = ['ASC', 'DESC']
   <%
   stages = default_sorting.sorts_on?(column) ? default_sorting_stages : sorting_stages
   column_sort_direction = stages.after(sorting.direction_of(column)) || 'ASC'
-  sort_params = params_for(:action => :update_table, :page => 1,
+  sort_params = params_for(:action => :index, :page => 1,
                            :sort => column.name, :sort_direction => column_sort_direction)
   column_header_id = active_scaffold_column_header_id(column)
   -%>
@@ -20,7 +20,6 @@ default_sorting_stages = ['ASC', 'DESC']
         :before => "addActiveScaffoldPageToHistory('#{href}', '#{controller_id}')",
         :loading => "Element.addClassName('#{column_header_id}','loading');",
         :failure => "ActiveScaffold.report_500_response('#{active_scaffold_id}')",
-        :update => active_scaffold_content_id,
         :method => :get },
       { :href => href } %>
    <% else -%>

frontends/default/views/_list_pagination_links.html.erb
@@ -1,5 +1,5 @@
 <% unless current_page.nil? -%>
-  <% pagination_params = params_for(:action => 'update_table') -%>
+  <% pagination_params = params_for(:action => :index) -%>
   <% indicator_params = pagination_params.merge(:action => 'pagination') -%>
   <% previous_url = url_for(pagination_params.merge(:page => current_page.number - 1)) -%>
   <% next_url = url_for(pagination_params.merge(:page => current_page.number + 1)) -%>
@@ -12,7 +12,6 @@
           :before => "addActiveScaffoldPageToHistory('#{previous_url}', '#{controller_id}');",
           :complete => "$('#{loading_indicator_id(:action => :pagination)}').style.visibility = 'hidden';",
           :failure => "ActiveScaffold.report_500_response('#{active_scaffold_id}')",
-          :update => active_scaffold_content_id,
           :method => :get },
         { :href => previous_url,
           :class => "previous"}) if current_page.prev? %>
@@ -23,7 +22,6 @@
           :before => "addActiveScaffoldPageToHistory('#{next_url}', '#{controller_id}');",
           :complete => "$('#{loading_indicator_id(:action => :pagination)}').style.visibility = 'hidden';",
           :failure => "ActiveScaffold.report_500_response('#{active_scaffold_id}')",
-          :update => active_scaffold_content_id,
           :method => :get },
         { :href => next_url,
           :class => "next"}) if current_page.next? %>

frontends/default/views/_live_search.html.erb
@@ -1,11 +1,10 @@
-<% href = url_for(params_for(:action => :update_table, :escape => false).delete_if{|k,v| k == 'search'}) -%>
+<% href = url_for(params_for(:action => :index, :escape => false).delete_if{|k,v| k == 'search'}) -%>
 <%= form_remote_tag :url => href,
                     :method => :get,
                     :before => "addActiveScaffoldPageToHistory('#{href}', '#{params[:controller]}')",
                     :after => "$('#{loading_indicator_id(:action => :search, :id => params[:id])}').style.visibility = 'visible';",
                     :complete => "$('#{loading_indicator_id(:action => :search, :id => params[:id])}').style.visibility = 'hidden';",
                     :failure => "ActiveScaffold.report_500_response('#{active_scaffold_id}')",
-                    :update => active_scaffold_content_id,
                     :html => { :action => href, :id => search_form_id, :class => 'search', :method => :get } %>
   <%= text_field_tag :search, params[:search], :class => 'text-input', :id => search_input_id, :size => 50, :autocompleted => :off %>
   <a href="javascript:void(0)" class="cancel" onclick="f = this.up('form'); f.reset(); f.onsubmit();"><%= as_(:reset) -%></a>

frontends/default/views/_search.html.erb
@@ -1,11 +1,10 @@
-<% href = url_for(params_for(:action => :update_table, :escape => false).delete_if{|k,v| k == 'search'}) -%>
+<% href = url_for(params_for(:action => :index, :escape => false).delete_if{|k,v| k == 'search'}) -%>
 <%= form_remote_tag :url => href,
                     :method => :get,
                     :before => "addActiveScaffoldPageToHistory('#{href}', '#{params[:controller]}')",
                     :after => "$('#{loading_indicator_id(:action => :search)}').style.visibility = 'visible'; Form.disable('#{search_form_id}');",
                     :complete => "$('#{loading_indicator_id(:action => :search)}').style.visibility = 'hidden'; Form.enable('#{search_form_id}');",
                     :failure => "ActiveScaffold.report_500_response('#{active_scaffold_id}')",
-                    :update => active_scaffold_content_id,
                     :html => { :action => href, :id => search_form_id, :class => 'search', :method => :get } %>
   <%= text_field_tag :search, params[:search], :class => 'text-input', :id => search_input_id, :size => 50, :autocompleted => :off %>
   <%= submit_tag as_(:search), :class => "submit" %>

commit 262225a04cec71d4506fff12f87640f82c4c4bac

Fix nested scaffolds broken in dd2f2c8

lib/active_scaffold/actions/list.rb
@@ -10,7 +10,7 @@ module ActiveScaffold::Actions
 
     def table
       do_list
-      render(:action => 'list', :layout => false)
+      render(:action => 'list.html', :layout => false)
     end
 
     # get just a single row