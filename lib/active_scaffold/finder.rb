0236d2952a10b7d52d3d1b02844f1e5ecfc703d5
scambra <sergio@entrecables.com>
2010-03-29 10:50:09 +0200

    Fix remembering value in calendar_date_select and datetime search ui

--- lib/active_scaffold/finder.rb
+++ lib/active_scaffold/finder.rb
@@ -87,7 +87,7 @@ module ActiveScaffold
       def condition_for_datetime_type(column, value, like_pattern = nil)
         conversion = value[:from][:hour].blank? && value[:to][:hour].blank? ? :to_date : :to_time
         from_value, to_value = [:from, :to].collect do |field|
-          Time.zone.local(*[:year, :month, :day, :hour, :minutes, :seconds].collect {|part| value[field][part].to_i}) rescue nil
+          Time.zone.local(*[:year, :month, :day, :hour, :minute, :second].collect {|part| value[field][part].to_i}) rescue nil
         end
 
         if from_value.nil? and to_value.nil?
