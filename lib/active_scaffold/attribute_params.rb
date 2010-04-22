aca81c20da12f3de9a013d69653d36e17af5e281
Chris Eppstein <chris@eppsteins.net>
2010-04-23 05:43:42 +0800

    Don't enforce this check on finder_sql associations

--- lib/active_scaffold/attribute_params.rb
+++ lib/active_scaffold/attribute_params.rb
@@ -73,7 +73,7 @@ module ActiveScaffold
 
       if parent_record.new_record?
         parent_record.class.reflect_on_all_associations.each do |a|
-          next unless [:has_one, :has_many].include?(a.macro) and not a.options[:through]
+          next unless [:has_one, :has_many].include?(a.macro) and not (a.options[:through] || a.options[:finder_sql])
           next unless association_proxy = parent_record.send(a.name)
 
           raise ActiveScaffold::ReverseAssociationRequired, "Association #{a.name}: In order to support :has_one and :has_many where the parent record is new and the child record(s) validate the presence of the parent, ActiveScaffold requires the reverse association (the belongs_to)." unless a.reverse
