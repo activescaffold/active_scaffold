551ab38ba9fa4ba8fe555298a757b94af98a4952
scambra <sergio@entrecables.com>
2010-05-07 13:20:30 +0200

    Allow to enable string comparators in search_ui, needed to search in a string column of an association

--- lib/active_scaffold/helpers/search_column_helpers.rb
+++ lib/active_scaffold/helpers/search_column_helpers.rb
@@ -128,7 +128,7 @@ module ActiveScaffold
       def active_scaffold_search_range(column, options)
         opt_value, from_value, to_value = field_search_params_range_values(column)
         select_options = ActiveScaffold::Finder::NumericComparators.collect {|comp| [as_(comp.downcase.to_sym), comp]}
-        select_options.unshift *ActiveScaffold::Finder::StringComparators.collect {|title, comp| [as_(title), comp]} if column.column && column.column.text?
+        select_options.unshift *ActiveScaffold::Finder::StringComparators.collect {|title, comp| [as_(title), comp]} if column.options[:string_comparators] || column.column && column.column.text?
 
         html = []
         html << select_tag("#{options[:name]}[opt]",
