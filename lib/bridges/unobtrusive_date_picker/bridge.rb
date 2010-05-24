33ebbf40e244f909711bcaabbadb9571735d0023
scambra <sergio@entrecables.com>
2010-05-24 11:07:57 +0200

    test unobtrusive date picker bridge

--- lib/bridges/unobtrusive_date_picker/bridge.rb
+++ lib/bridges/unobtrusive_date_picker/bridge.rb
@@ -3,5 +3,6 @@ ActiveScaffold.bridge "UnobtrusiveDatePicker" do
     require File.join(File.dirname(__FILE__), "lib/unobtrusive_date_picker_bridge.rb")
     require File.join(File.dirname(__FILE__), "lib/form_ui.rb")
     require File.join(File.dirname(__FILE__), "lib/view_helpers.rb")
+    ActiveScaffold::Config::Core.send :include, ActiveScaffold::UnobtrusiveDatePickerBridge
   end
 end
