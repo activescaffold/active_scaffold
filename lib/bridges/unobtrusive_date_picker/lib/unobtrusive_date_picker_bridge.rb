bdfc4b7a92da2b407239073d705f182665e9e9e4
scambra <sergio@entrecables.com>
2010-05-24 11:07:57 +0200

    test unobtrusive date picker bridge

--- lib/bridges/unobtrusive_date_picker/lib/unobtrusive_date_picker_bridge.rb
+++ lib/bridges/unobtrusive_date_picker/lib/unobtrusive_date_picker_bridge.rb
@@ -1,12 +1,15 @@
-module ActiveScaffold::Config
-  class Core < Base
+module ActiveScaffold
+  module UnobtrusiveDatePickerBridge
     def initialize_with_unobtrusive_date_picker(model_id)
       initialize_without_unobtrusive_date_picker(model_id)
       date_fields = self.model.columns.select {|c| [:date, :datetime].include?(c.type) }
-      
+
       # automatically set the forum_ui to a file column
       date_fields.each {|field| self.columns[field.name.to_sym].form_ui = :datepicker}
     end
-    alias_method_chain :initialize, :unobtrusive_date_picker
+
+    def self.included(base)
+      base.alias_method_chain :initialize, :unobtrusive_date_picker
+    end
   end
 end
