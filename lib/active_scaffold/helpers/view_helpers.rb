ffa701e5101597775a07d7c1d045573f3bd83a8a
vhochstein <v.hochstein@highstone.de>
2010-05-28 12:50:56 +0200

    move error_message_for into activescaffold to remove dependency on dynamic form plugin

--- lib/active_scaffold/helpers/view_helpers.rb
+++ lib/active_scaffold/helpers/view_helpers.rb
@@ -217,6 +217,62 @@ module ActiveScaffold
         value = false unless record.class.authorized_for?(:crud_type => :create)
         value
       end
+      
+      def error_messages_for(*params)
+        options = params.extract_options!.symbolize_keys
+
+        objects = Array.wrap(options.delete(:object) || params).map do |object|
+          object = instance_variable_get("@#{object}") unless object.respond_to?(:to_model)
+          object = convert_to_model(object)
+
+          if object.class.respond_to?(:model_name)
+            options[:object_name] ||= object.class.model_name.human.downcase
+          end
+
+          object
+        end
+
+        objects.compact!
+        count = objects.inject(0) {|sum, object| sum + object.errors.count }
+
+        unless count.zero?
+          html = {}
+          [:id, :class].each do |key|
+            if options.include?(key)
+              value = options[key]
+              html[key] = value unless value.blank?
+            else
+              html[key] = 'errorExplanation'
+            end
+          end
+          options[:object_name] ||= params.first
+
+          I18n.with_options :locale => options[:locale], :scope => [:activerecord, :errors, :template] do |locale|
+            header_message = if options.include?(:header_message)
+              options[:header_message]
+            else
+              locale.t :header, :count => count, :model => options[:object_name].to_s.gsub('_', ' ')
+            end
+
+            message = options.include?(:message) ? options[:message] : locale.t(:body)
+
+            error_messages = objects.sum do |object|
+              object.errors.full_messages.map do |msg|
+                content_tag(:li, msg)
+              end
+            end.join.html_safe
+
+            contents = ''
+            contents << content_tag(options[:header_tag] || :h2, header_message) unless header_message.blank?
+            contents << content_tag(:p, message) unless message.blank?
+            contents << content_tag(:ul, error_messages)
+
+            content_tag(:div, contents.html_safe, html)
+          end
+        else
+          ''
+        end
+      end
     end
   end
 end
