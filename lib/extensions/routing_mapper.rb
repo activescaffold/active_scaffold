f04946396e6af666edd43fb5a8b651c84ff84e34
vhochstein <v.hochstein@highstone.de>
2010-06-02 11:57:12 +0200

    improved as_routing definition: resources :teams do   as_routes end

---
+++ lib/extensions/routing_mapper.rb
@@ -0,0 +1,29 @@
+module ActionDispatch
+  module Routing
+    class Mapper
+      module Base
+        def as_routes(options = {:full => false})
+          collection do 
+            get :show_search, :list, :render_field
+          end
+          member do
+            get :row, :nested, :render_field, :delete
+            post :update_column
+          end
+          as_extended_routes if options[:full]
+        end
+        
+        def as_extended_routes
+          collection do 
+            get :edit_associated, :new_existing
+            post :add_existing
+          end
+          member do
+            get :edit_associated, :add_association
+            delete :destroy_existing
+          end
+        end
+      end
+    end
+  end
+end
