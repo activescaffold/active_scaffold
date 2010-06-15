69e2db0767607bfbbd88ada517eb00ae73b610fe
vhochstein <v.hochstein@highstone.de>
2010-06-15 16:00:22 +0200

    get render :super up and running

--- lib/extensions/action_view_rendering.rb
+++ lib/extensions/action_view_rendering.rb
@@ -1,3 +1,18 @@
+module ActionView
+  class LookupContext
+    module ViewPaths
+      def find_all_templates(name, prefix = nil, partial = false)
+        templates = []
+        @view_paths.each do |resolver|
+          template = resolver.find_all(*args_for_lookup(name, prefix, partial)).first
+          templates << template unless template.nil?
+        end
+        templates
+      end
+    end
+  end
+end
+
 # wrap the action rendering for ActiveScaffold views
 module ActionView::Rendering #:nodoc:
   # Adds two rendering options.
@@ -25,24 +40,11 @@ module ActionView::Rendering #:nodoc:
     if args.first == :super
       options = args[1] || {}
       options[:locals] ||= {}
-      options[:locals].reverse_merge! @local_assigns
-
-      known_extensions = [:erb, :rhtml, :rjs, :haml]
-      # search through call stack for a template file (normally matches on first caller)
-      # note that we can't use split(':').first because windoze boxen may have an extra colon to specify the drive letter. the
-      # solution is to count colons from the *right* of the string, not the left. see issue #299.
-      template_path = caller.find{|c| known_extensions.include?(c.split(':')[-3].split('.').last.to_sym) }
-      template = File.basename(template_path.split(':')[-3])
-      template, format = template.split('.')
-
-      # paths previous to current template_path must be ignored to avoid infinite loops when is called twice or more
-      index = 0
-      controller.class.active_scaffold_paths.each_with_index do |active_scaffold_template_path, i|
-        index = i + 1 and break if template_path.include? active_scaffold_template_path
-      end
-
-      active_scaffold_template = controller.class.active_scaffold_paths.slice(index..-1).find_template(template, format, false)
-      render(:file => active_scaffold_template, :locals => options[:locals])
+      options[:locals].reverse_merge!(@last_partial[:locals] || {})
+      templates = lookup_context.find_all_templates(@last_partial[:partial], nil, true)
+      @last_partial[:index] = @last_partial[:index].nil? ? 0 : @last_partial[:index] + 1  
+      options[:template] = templates[@last_partial[:index]]
+      render options
     elsif args.first.is_a?(Hash) and args.first[:active_scaffold]
       require 'digest/md5'
       options = args.first
@@ -63,11 +65,15 @@ module ActionView::Rendering #:nodoc:
       end
       #render_component :controller => remote_controller.to_s, :action => 'table', :params => options[:params]
     else
+      options = args.first
+      @last_partial = {:partial => options[:partial], :index => nil} if options[:partial]
+      @last_partial[:locals] = options[:locals] if options[:locals]
       render_without_active_scaffold(*args, &block)
     end
   end
   alias_method_chain :render, :active_scaffold
   
+
   def partial_pieces(partial_path)
     if partial_path.include?('/')
       return File.dirname(partial_path), File.basename(partial_path)
