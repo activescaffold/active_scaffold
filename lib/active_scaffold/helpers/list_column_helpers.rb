de22fb8238391ce9eea577ba21751acfae98b6b7
scambra <sergio@entrecables.com>
2010-06-16 14:14:09 +0200

    Add column.update_column and deprecate column.options[:update_column]

--- lib/active_scaffold/helpers/list_column_helpers.rb
+++ lib/active_scaffold/helpers/list_column_helpers.rb
@@ -299,8 +299,7 @@ module ActiveScaffold
         if inplace_edit?(active_scaffold_config.model, column) and inplace_edit_cloning?(column)
           @record = active_scaffold_config.model.new
           column = column.clone
-          column.options = column.options.clone
-          column.options.delete(:update_column)
+          column.update_column = nil
           column.form_ui = :select if (column.association && column.form_ui.nil?)
           content_tag(:div, active_scaffold_input_for(column), {:style => "display:none;", :class => inplace_edit_control_css_class})
         end
