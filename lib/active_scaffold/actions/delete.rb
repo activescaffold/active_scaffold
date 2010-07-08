68f3c9ef33c0b9a5e50b0eca957621e43b78183e
scambra <sergio@entrecables.com>
2010-07-08 08:33:40 +0200

    Remove id from marked records when record is deleted

--- lib/active_scaffold/actions/delete.rb
+++ lib/active_scaffold/actions/delete.rb
@@ -49,7 +49,7 @@ module ActiveScaffold::Actions
       destroy_find_record
       begin
         self.successful = @record.destroy
-        marked_records.delete @record.id if successful?
+        marked_records.delete @record.id.to_s if successful?
       rescue
         flash[:warning] = as_(:cant_destroy_record, :record => @record.to_label)
         self.successful = false
