0ed2117f74eee88014a94f64288f2e30b0f0a1af
vhochstein <v.hochstein@highstone.de>
2010-07-15 09:12:35 +0200

    do not allow create, update, delete if association is readonly or if it is a through association

--- lib/active_scaffold/actions/update.rb
+++ lib/active_scaffold/actions/update.rb
@@ -112,7 +112,7 @@ module ActiveScaffold::Actions
     # The default security delegates to ActiveRecordPermissions.
     # You may override the method to customize.
     def update_authorized?(record = nil)
-      authorized_for?(:crud_type => :update)
+      (!nested? || !nested.readonly?) && authorized_for?(:crud_type => :update)
     end
     private
     def update_authorized_filter
