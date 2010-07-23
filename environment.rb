e8b69c96440b5fe70087465b3d8f955717fa7dd1
vhochstein <v.hochstein@highstone.de>
2010-07-23 11:12:14 +0200

    add configuration option to switch js_framework

--- environment.rb
+++ environment.rb
@@ -15,3 +15,4 @@ ActiveRecord::Base.class_eval     {include ActiveRecordPermissions::Permissions}
 require 'bridges/bridge.rb'
 
 I18n.load_path += Dir[File.join(File.dirname(__FILE__), 'lib', 'active_scaffold', 'locale', '*.{rb,yml}')]
+#ActiveScaffold.js_framework = :jquery
