aa97776eb11ffe5b2572bf53ae9a273292ac23db
vhochstein <v.hochstein@highstone.de>
2010-08-02 12:25:47 +0200

    Fixed bridges autoloading issues with Rails 3.0 rc1

---
+++ lib/active_scaffold/bridges/paperclip/bridge.rb
@@ -0,0 +1,10 @@
+ActiveScaffold::Bridges.bridge "Paperclip" do
+  install do
+    if ActiveScaffold::Config::Core.instance_methods.include?("initialize_with_paperclip")
+      raise RuntimeError, "We've detected that you have active_scaffold_paperclip_bridge installed. This plugin has been moved to core. Please remove active_scaffold_paperclip_bridge to prevent any conflicts"
+    end
+    require File.join(File.dirname(__FILE__), "lib/form_ui")
+    require File.join(File.dirname(__FILE__), "lib/list_ui")
+    ActiveScaffold::Config::Core.send :include, ActiveScaffold::Bridges::Paperclip::Lib::PaperclipBridge
+  end
+end
