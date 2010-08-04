5e22abcb65d2b32a2e66ae302ac27a90b75dac15
vhochstein <v.hochstein@highstone.de>
2010-08-04 10:01:54 +0200

    jquery: added a bridge to use jquery_ui DatePicker for date attributes and http://trentrichardson.com/examples/timepicker/ for dateTime attributes

---
+++ lib/active_scaffold/bridges/date_picker/bridge.rb
@@ -0,0 +1,19 @@
+ActiveScaffold::Bridges.bridge "DatePicker" do
+  install do
+    directory = File.dirname(__FILE__)
+    source = File.join(directory, "public/javascripts/date_picker_bridge.js")
+    destination = File.join(Rails.root, "public/javascripts/active_scaffold/default/")
+
+    if ActiveScaffold.js_framework == :jquery
+      require File.join(directory, "lib/datepicker_bridge.rb")
+      FileUtils.cp(source, destination)
+    else
+      # make sure that jquery files are removed
+      FileUtils.rm(File.join(destination, 'date_picker_bridge.js')) if File.exist?(File.join(destination, 'date_picker_bridge.js'))
+    end
+  end
+  
+  install? do
+    true
+  end
+end
