ac64076bfd2e64b117842b96b1969f78ef7b5e0d
scambra <sergio@entrecables.com>
2010-09-15 17:57:41 +0200

    Core config test

--- lib/active_scaffold/config/core.rb
+++ lib/active_scaffold/config/core.rb
@@ -91,7 +91,7 @@ module ActiveScaffold::Config
     # a generally-applicable name for this ActiveScaffold ... will be used for generating page/section headers
     attr_writer :label
     def label(options={})
-      as_(@label, options) || model.human_name(options.merge(options[:count].to_i == 1 ? {} : {:default => model.name.pluralize}))
+      as_(@label, options) || model.human_name(options.merge(:default => options[:count].to_i == 1 ? model.name : model.name.pluralize))
     end
 
     # STI children models, use an array of model names
