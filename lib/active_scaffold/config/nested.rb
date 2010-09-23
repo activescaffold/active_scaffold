eb4bec5d858dcd7d27d99daee4832a369e9b579e
scambra <sergio@entrecables.com>
2010-09-23 18:06:36 +0200

    Add deprecation warning about nested.add_link multiple associations

--- lib/active_scaffold/config/nested.rb
+++ lib/active_scaffold/config/nested.rb
@@ -18,11 +18,20 @@ module ActiveScaffold::Config
     attr_accessor :shallow_delete
 
     # Add a nested ActionLink
-    def add_link(label, models, options = {})
+    def add_link(label, association, options = {})
+      if association.is_a? Array
+        msg = "config.nested.add_link with multiple associations is not already supported. "
+        if association.size == 1
+          ::ActiveSupport::Deprecation.warn(msg + "Remove array", caller)
+        else
+          ::ActiveSupport::Deprecation.warn(msg + "The first model will be used", caller)
+        end
+        association = association.first
+      end
       options.reverse_merge! :security_method => :nested_authorized?, :position => :after
-      options.merge! :label => label, :type => :member, :parameters => {:associations => models.join(' ')}
+      options.merge! :label => label, :type => :member, :parameters => {:associations => association}
       options[:html_options] ||= {}
-      options[:html_options][:class] = [options[:html_options][:class], models.join(' ')].compact.join(' ')
+      options[:html_options][:class] = [options[:html_options][:class], association].compact.join(' ')
       @core.action_links.add('nested', options)
     end
 
--- test/config/nested_test.rb
+++ test/config/nested_test.rb
@@ -24,8 +24,8 @@ class Config::NestedTest < Test::Unit::TestCase
     assert @config.nested.shallow_delete
   end
   
-  def test_add_link
-    @config.nested.add_link :custom_link, [:assoc_1, :assoc_2]
+  def test_add_link_deprecation
+    ActiveSupport::Deprecation.silence { @config.nested.add_link :custom_link, [:assoc_1, :assoc_2] }
     link = @config.action_links['nested']
     assert_equal 'Custom Link', link.label
     assert_equal 'nested', link.action
@@ -34,8 +34,26 @@ class Config::NestedTest < Test::Unit::TestCase
     assert !link.popup?
     assert !link.confirm?
     assert link.inline?
-    assert_equal 'assoc_1 assoc_2', link.parameters[:associations]
-    assert_equal 'assoc_1 assoc_2', link.html_options[:class]
+    assert_equal :assoc_1, link.parameters[:associations]
+    assert_equal 'assoc_1', link.html_options[:class]
+    assert_equal :get, link.method
+    assert_equal :member, link.type
+    assert_equal :read, link.crud_type
+    assert_equal :nested_authorized?, link.security_method
+  end
+
+  def test_add_link
+    @config.nested.add_link :custom_link, :assoc_1
+    link = @config.action_links['nested']
+    assert_equal 'Custom Link', link.label
+    assert_equal 'nested', link.action
+    assert_equal :after, link.position
+    assert !link.page?
+    assert !link.popup?
+    assert !link.confirm?
+    assert link.inline?
+    assert_equal :assoc_1, link.parameters[:associations]
+    assert_equal 'assoc_1', link.html_options[:class]
     assert_equal :get, link.method
     assert_equal :member, link.type
     assert_equal :read, link.crud_type
