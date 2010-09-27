76275eadf2fc38ec5db28e95845c40e972a3748a
vhochstein <v.hochstein@highstone.de>
2010-09-27 16:35:17 +0200

    improved and simplified action link response management

--- lib/active_scaffold/actions/list.rb
+++ lib/active_scaffold/actions/list.rb
@@ -77,6 +77,22 @@ module ActiveScaffold::Actions
     def list_authorized?
       authorized_for?(:crud_type => :read)
     end
+    
+    def action_update_respond_to_js
+      render(:action => 'on_action_update')
+    end
+
+    def action_update_respond_to_xml
+      render :xml => successful? ? "" : response_object.to_xml(:only => active_scaffold_config.list.columns.names), :content_type => Mime::XML, :status => response_status
+    end
+
+    def action_update_respond_to_json
+      render :text => successful? ? "" : response_object.to_json(:only => active_scaffold_config.list.columns.names), :content_type => Mime::JSON, :status => response_status
+    end
+
+    def action_update_respond_to_yaml
+      render :text => successful? ? "" : Hash.from_xml(response_object.to_xml(:only => active_scaffold_config.list.columns.names)).to_yaml, :content_type => Mime::YAML, :status => response_status
+    end
      
     private
     def list_authorized_filter
@@ -84,6 +100,9 @@ module ActiveScaffold::Actions
     end
     def list_formats
       (default_formats + active_scaffold_config.formats + active_scaffold_config.list.formats).uniq
+    end
+    def action_update_formats
+      (default_formats + active_scaffold_config.formats).uniq
     end
   end
 end
