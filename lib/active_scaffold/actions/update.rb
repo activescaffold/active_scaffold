a538b28b6a2f91e66d3789fe196b8f154d65035c
vhochstein <v.hochstein@highstone.de>
2010-09-29 16:50:53 +0200

    Bugfix: update form should nt show parent column

--- lib/active_scaffold/actions/update.rb
+++ lib/active_scaffold/actions/update.rb
@@ -63,6 +63,7 @@ module ActiveScaffold::Actions
     # A simple method to find and prepare a record for editing
     # May be overridden to customize the record (set default values, etc.)
     def do_edit
+      register_constraints_with_action_columns(nested.constrained_fields) if nested?
       @record = find_if_allowed(params[:id], :update)
     end
 
