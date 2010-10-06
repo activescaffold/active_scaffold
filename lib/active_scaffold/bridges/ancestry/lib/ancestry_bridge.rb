ebd8426a368f45ceb8abd4fd224427b558d51382
vhochstein <v.hochstein@highstone.de>
2010-10-06 11:10:54 +0200

    added ancestry plugin bridge

---
+++ lib/active_scaffold/bridges/ancestry/lib/ancestry_bridge.rb
@@ -0,0 +1,38 @@
+ActiveScaffold::Config::Core.class_eval do
+  def initialize_with_ancestry(model_id)
+    initialize_without_ancestry(model_id)
+
+    return unless self.model.singleton_methods.include?('has_ancestry')
+
+    col_config = self.columns[self.model.ancestry_column]
+    unless col_config.nil?
+      col_config.form_ui = :ancestry
+      create.columns.exclude :ancestry
+      list.columns.exclude :ancestry
+    end
+  end
+
+  alias_method_chain :initialize, :ancestry
+end
+
+module ActiveScaffold
+  module AncestryBridge
+    module FormColumnHelpers
+      def active_scaffold_input_ancestry(column, options)
+        select_options = []
+        traverse_ancestry = lambda do|key, value|
+          unless key == @record
+            select_options << ["#{'__' * key.depth}#{key.to_label}", key.id]
+            value.each(&traverse_ancestry) if value.is_a?(Hash) && !value.empty?
+          end
+        end
+        @record.class.arrange.each(&traverse_ancestry)
+        select(:record, :ancestry, select_options, { :selected => @record.send(:ancestry) }, options)
+      end
+    end
+  end
+end
+
+ActionView::Base.class_eval do
+  include ActiveScaffold::AncestryBridge::FormColumnHelpers
+end
