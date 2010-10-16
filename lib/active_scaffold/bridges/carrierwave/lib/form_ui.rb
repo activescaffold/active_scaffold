ca3fb608bc9d18f2cc719338a07d1cf42a6124f8
Nicolae Claudius <clyfe@ubuntu.(none)>
2010-10-16 10:59:11 -0700

    allow carrierwave bridge to work with other storages besides FS

--- lib/active_scaffold/bridges/carrierwave/lib/form_ui.rb
+++ lib/active_scaffold/bridges/carrierwave/lib/form_ui.rb
@@ -5,20 +5,27 @@ module ActiveScaffold
         options = active_scaffold_input_text_options(options)
         input = file_field(:record, column.name, options)
         carrierwave = @record.send("#{column.name}")
-        if carrierwave.current_path.present? && File.exist?(carrierwave.current_path)
+        if carrierwave.file.present? && !carrierwave.file.empty?
           if ActiveScaffold.js_framework == :jquery
             js_remove_file_code = "$(this).prev().val('true'); $(this).parent().hide().next().show(); return false;";
           else
             js_remove_file_code = "$(this).previous().value='true'; $(this).up().hide().next().show(); return false;";
           end
-          
-          content = active_scaffold_column_carrierwave(column, @record)
-          content_tag(:div,
-            (content + " | " +
-              hidden_field(:record, "delete_#{column.name}", :value => "false") +
-              content_tag(:a, as_(:remove_file), {:href => '#', :onclick => js_remove_file_code})
-            ).html_safe
-          ) + content_tag(:div, input, :style => "display: none")
+
+          hidden_field_options = {
+            :name => options[:name].gsub(/\[#{column.name}\]$/, "[delete_#{column.name}]"),
+            :id => options[:id] + '_delete',
+            :value => "false"
+          }
+
+          content_tag( :div,
+            content_tag(:div, (
+                get_column_value(@record, column) + " | " +
+                  hidden_field(:record, "delete_#{column.name}", hidden_field_options) +
+                  content_tag(:a, as_(:remove_file), {:href => '#', :onclick => js_remove_file_code})
+              ).html_safe
+            ) + content_tag(:div, input, :style => "display: none")
+          )
         else
           input
         end
--- lib/active_scaffold/bridges/carrierwave/lib/list_ui.rb
+++ lib/active_scaffold/bridges/carrierwave/lib/list_ui.rb
@@ -3,7 +3,7 @@ module ActiveScaffold
     module ListColumnHelpers
       def active_scaffold_column_carrierwave(column, record)
         carrierwave = record.send("#{column.name}")
-        return nil unless carrierwave.current_path.present? && File.exist?(carrierwave.current_path)
+        return nil unless carrierwave.file.present? && !carrierwave.file.empty?
         thumbnail_style = ActiveScaffold::Bridges::Carrierwave::Lib::CarrierwaveBridgeHelpers.thumbnail_style
         content = if carrierwave.versions.keys.include?(thumbnail_style)
           image_tag(carrierwave.url(thumbnail_style), :border => 0).html_safe
