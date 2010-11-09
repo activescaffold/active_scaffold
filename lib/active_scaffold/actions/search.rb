953816a017bab852ff8b2c42ade395224d50e931
vhochstein <v.hochstein@highstone.de>
2010-11-09 13:51:48 +0100

    search before_filters only for action :index

--- lib/active_scaffold/actions/search.rb
+++ lib/active_scaffold/actions/search.rb
@@ -3,8 +3,8 @@ module ActiveScaffold::Actions
     include ActiveScaffold::Actions::CommonSearch
     def self.included(base)
       base.before_filter :search_authorized_filter, :only => :show_search
-      base.before_filter :store_search_params_into_session, :only => [:list, :index]
-      base.before_filter :do_search, :only => [:list, :index]
+      base.before_filter :store_search_params_into_session, :only => [:index]
+      base.before_filter :do_search, :only => [:index]
       base.helper_method :search_params
     end
 
