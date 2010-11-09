6372af05b1a34fc090abe655bf143de81e29f726
scambra <sergio@entrecables.com>
2010-11-09 12:15:01 +0100

    fix removing all associated record when show_blank_record is disabled

--- lib/active_scaffold/attribute_params.rb
+++ lib/active_scaffold/attribute_params.rb
@@ -61,12 +61,7 @@ module ActiveScaffold
           # we avoid assigning a value that already exists because otherwise has_one associations will break (AR bug in has_one_association.rb#replace)
           parent_record.send("#{column.name}=", value) unless parent_record.send(column.name) == value
           
-        # plural associations may not actually appear in the params if all of the options have been unselected or cleared away.
-        # the "form_ui" check is necessary, becuase without it we have problems
-        # with subforms. the UI cuts out deep associations, which means they're not present in the
-        # params even though they're in the columns list. the result is that associations were being
-        # emptied out way too often.
-        elsif column.form_ui and column.plural_association?
+        elsif column.plural_association?
           parent_record.send("#{column.name}=", [])
         end
       end
