aa313ae50421ddf0c4d426abfc9ebd1e0928a25a
vhochstein <v.hochstein@highstone.de>
2010-11-24 11:38:37 +0100

    traverse action_links: pass current level to block

--- lib/active_scaffold/data_structures/action_links.rb
+++ lib/active_scaffold/data_structures/action_links.rb
@@ -78,21 +78,24 @@ module ActiveScaffold::DataStructures
 
     def traverse(controller, options = {}, &block)
       traverse_method = options.delete(:reverse).nil? ? :each : :reverse_each
+      options[:level] ||= -1
+      options[:level] += 1
       first_action = true
       @set.send(traverse_method) do |link|
         if link.is_a?(ActiveScaffold::DataStructures::ActionLinks)
           # add top node only if there is anything in the list
           #yield({:kind => :node, :level => 1, :last => false, :link => link})
-          yield(link, nil, {:node => :start_traversing, :first_action => first_action})
+          yield(link, nil, {:node => :start_traversing, :first_action => first_action, :level => options[:level]})
           link.traverse(controller,options, &block)
-          yield(link, nil, {:node => :finished_traversing, :first_action => first_action})
+          yield(link, nil, {:node => :finished_traversing, :first_action => first_action, :level => options[:level]})
           #yield({:kind => :completed_group, :level => 1, :last => false, :link => link})
         elsif controller.nil? || !skip_action_link(controller, link, *(Array(options[:record])))
           authorized = options[:for].nil? ? true : options[:for].authorized_for?(:crud_type => link.crud_type, :action => link.action)
-          yield(self, link, {:authorized => authorized, :first_action => first_action})
+          yield(self, link, {:authorized => authorized, :first_action => first_action, :level => options[:level]})
         end
         first_action = false
       end
+      options[:level] -= 1
     end
 
     def collect
