112341cf0e2acddb02b3d61f3cb0722b00f4a57a
vhochstein <v.hochstein@highstone.de>
2010-12-28 10:01:26 +0100

    Bugfix: ancestry_bridge fix updating parent

--- lib/active_scaffold/bridges/ancestry/lib/ancestry_bridge.rb
+++ lib/active_scaffold/bridges/ancestry/lib/ancestry_bridge.rb
@@ -3,13 +3,12 @@ ActiveScaffold::Config::Core.class_eval do
     initialize_without_ancestry(model_id)
 
     return unless self.model.respond_to? :ancestry_column
-
-    col_config = self.columns[self.model.ancestry_column]
-    unless col_config.nil?
-      col_config.form_ui = :ancestry
-      create.columns.exclude :ancestry
-      list.columns.exclude :ancestry
-    end
+    
+    self.columns << :parent_id
+    self.columns[:parent_id].form_ui = :ancestry
+    update.columns.exclude :ancestry
+    create.columns.exclude :ancestry, :parent_id
+    list.columns.exclude :ancestry, :parent_id
   end
 
   alias_method_chain :initialize, :ancestry
@@ -20,6 +19,8 @@ module ActiveScaffold
     module FormColumnHelpers
       def active_scaffold_input_ancestry(column, options)
         select_options = []
+        select_control_options = {:selected => @record.parent_id}
+        select_control_options[:include_blank] = as_(:_select_) if @record.parent_id.nil?
         traverse_ancestry = proc do|key, value|
           unless key == @record
             select_options << ["#{'__' * key.depth}#{key.to_label}", key.id]
@@ -27,7 +28,7 @@ module ActiveScaffold
           end
         end
         @record.class.arrange.each(&traverse_ancestry)
-        select(:record, :ancestry, select_options, { :selected => @record.send(:ancestry) }, options)
+        select(:record, :ancestry, select_options, select_control_options, options)
       end
     end
   end
