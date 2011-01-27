568659104174d070510c56593c372fd3a612f1a3
vhochstein <v.hochstein@highstone.de>
2011-01-27 09:48:07 +0100

    Bugfix: uninitialized constant error in bridges (issue: 80 reported by korobkov)

--- lib/active_scaffold/bridges/carrierwave/bridge.rb
+++ lib/active_scaffold/bridges/carrierwave/bridge.rb
@@ -2,6 +2,8 @@ ActiveScaffold::Bridges.bridge "CarrierWave" do
   install do
     require File.join(File.dirname(__FILE__), "lib/form_ui")
     require File.join(File.dirname(__FILE__), "lib/list_ui")
+    require File.join(File.dirname(__FILE__), "lib/carrierwave_bridge_helpers")
+    require File.join(File.dirname(__FILE__), "lib/carrierwave_bridge")
     ActiveScaffold::Config::Core.send :include, ActiveScaffold::Bridges::Carrierwave::Lib::CarrierwaveBridge
   end
 end
