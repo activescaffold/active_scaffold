c5bb14bba0ef820595aaa5a6e1242f7f65ba6c58
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-02-16 15:34:10 +0200

    refactor carrierwave bridge

--- lib/active_scaffold/bridges/carrierwave/lib/carrierwave_bridge_helpers.rb
+++ lib/active_scaffold/bridges/carrierwave/lib/carrierwave_bridge_helpers.rb
@@ -5,20 +5,6 @@ module ActiveScaffold
         module CarrierwaveBridgeHelpers
           mattr_accessor :thumbnail_style
           self.thumbnail_style = :thumbnail
-        
-          def self.generate_delete_helper(klass, field)
-            klass.class_eval <<-EOF, __FILE__, __LINE__ + 1 unless klass.methods.include?("delete_#{field}=")
-              attr_reader :delete_#{field}
-        
-              def delete_#{field}=(value)
-                value = (value == "true") if String === value
-                return unless value
-        
-                # passing nil to the file column causes the file to be deleted. Don't delete if we just uploaded a file!
-                self.remove_#{field}! unless self.#{field}.cached?.present?
-              end
-            EOF
-          end
         end
       end
     end
