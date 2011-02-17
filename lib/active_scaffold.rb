0a0c75c116708f0bb25cf2c148df7dce49992f39
vhochstein <v.hochstein@highstone.de>
2011-02-17 16:00:58 +0100

    active_scaffold_session_storage accepts a controller id

--- lib/active_scaffold.rb
+++ lib/active_scaffold.rb
@@ -81,8 +81,7 @@ module ActiveScaffold
     self.class.active_scaffold_config_for(klass)
   end
 
-  def active_scaffold_session_storage
-    id = params[:eid] || params[:controller]
+  def active_scaffold_session_storage(id = (params[:eid] || params[:controller]))
     session_index = "as:#{id}"
     session[session_index] ||= {}
     session[session_index]
