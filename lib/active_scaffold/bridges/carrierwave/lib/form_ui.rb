fc049369aadf6ddc7b1920ab054435d1c416c8c4
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-03-02 16:32:37 +0200

    fix carrierwave bug: "changing file on edit delets both old one and new one" this bug was introduced in my last carrierwave bridge commit (refactor)

--- lib/active_scaffold/bridges/carrierwave/lib/form_ui.rb
+++ lib/active_scaffold/bridges/carrierwave/lib/form_ui.rb
@@ -3,14 +3,8 @@ module ActiveScaffold
     module FormColumnHelpers
       def active_scaffold_input_carrierwave(column, options)
         options = active_scaffold_input_text_options(options)
-        input = file_field(:record, column.name, options)
         carrierwave = @record.send("#{column.name}")
         if carrierwave.file.present? && !carrierwave.file.empty?
-          if ActiveScaffold.js_framework == :jquery
-            js_remove_file_code = "$(this).prev().val('true'); $(this).parent().hide().next().show(); return false;";
-          else
-            js_remove_file_code = "$(this).previous().value='true'; $(this).up().hide().next().show(); return false;";
-          end
 
           remove_field_options = {
             :name => options[:name].gsub(/\[#{column.name}\]$/, "[remove_#{column.name}]"),
@@ -23,6 +17,15 @@ module ActiveScaffold
             :id => options[:id] + '_cache'
           }
 
+          if ActiveScaffold.js_framework == :jquery
+            js_remove_file_code = "$(this).prev('input#remove_#{options[:id]}').val('true'); $(this).parent().hide().next().show(); return false;";
+            js_dont_remove_file_code = "$(this).parents('div.carrierwave_controls').find('input#remove_#{options[:id]}').val('false'); return false;";
+          else
+            js_remove_file_code = "$(this).previous('input#remove_#{options[:id]}').value='true'; $(this).up().hide().next().show(); return false;";
+            js_dont_remove_file_code = "$(this).up('div.carrierwave_controls').down('input#remove_#{options[:id]}').value='false'; return false;";
+          end
+
+          input = file_field(:record, column.name, options.merge(:onchange => js_dont_remove_file_code))
           content_tag( :div,
             content_tag(:div, (
                 get_column_value(@record, column) + " | " +
@@ -30,10 +33,11 @@ module ActiveScaffold
                   hidden_field(:record, "remove_#{column.name}", remove_field_options) +
                   content_tag(:a, as_(:remove_file), {:href => '#', :onclick => js_remove_file_code})
               ).html_safe
-            ) + content_tag(:div, input, :style => "display: none")
+            ) + content_tag(:div, input, :style => "display: none"),
+            :class => 'carrierwave_controls'
           )
         else
-          input
+          file_field(:record, column.name, options)
         end
       end
     end
