3681f0fa03e82d4be0b9bb2284d6ce1e7e78dacb
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-03-04 14:56:45 +0200

    refactor edit_associated in AS do_action style to allow more granular overrides

--- lib/active_scaffold/actions/subform.rb
+++ lib/active_scaffold/actions/subform.rb
@@ -1,6 +1,13 @@
 module ActiveScaffold::Actions
   module Subform
     def edit_associated
+      do_edit_associated
+      render :action => 'edit_associated'
+    end
+
+    protected
+
+    def do_edit_associated
       @parent_record = params[:id].nil? ? new_model : find_if_allowed(params[:id], :update)
       @column = active_scaffold_config.columns[params[:association]]
 
@@ -14,8 +21,7 @@ module ActiveScaffold::Actions
 
       @scope = "[#{@column.name}]"
       @scope += (@record.new_record?) ? "[#{(Time.now.to_f*1000).to_i.to_s}]" : "[#{@record.id}]" if @column.plural_association?
-
-      render :action => 'edit_associated'
     end
+
   end
 end
