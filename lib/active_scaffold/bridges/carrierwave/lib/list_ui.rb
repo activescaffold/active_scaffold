b6206daa7c43f5879fcd560f29112b35cbfc0f3d
Nicolae Claudius <nicolae_claudius@yahoo.com>
2011-03-10 22:53:43 +0200

    carrierwave tiny refactor

--- lib/active_scaffold/bridges/carrierwave/lib/list_ui.rb
+++ lib/active_scaffold/bridges/carrierwave/lib/list_ui.rb
@@ -3,7 +3,7 @@ module ActiveScaffold
     module ListColumnHelpers
       def active_scaffold_column_carrierwave(column, record)
         carrierwave = record.send("#{column.name}")
-        return nil unless carrierwave.file.present? && !carrierwave.file.empty?
+        return nil unless !carrierwave.file.blank?
         thumbnail_style = ActiveScaffold::Bridges::Carrierwave::Lib::CarrierwaveBridgeHelpers.thumbnail_style
         content = if carrierwave.versions.keys.include?(thumbnail_style)
           image_tag(carrierwave.url(thumbnail_style), :border => 0).html_safe
