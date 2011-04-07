79420a6c07bf6dcfd2c348ecb97c7a1494f6a0b6
vhochstein <v.hochstein@highstone.de>
2011-04-07 21:31:00 +0200

    Bugfix: search range controls define a default range option

--- lib/active_scaffold/helpers/search_column_helpers.rb
+++ lib/active_scaffold/helpers/search_column_helpers.rb
@@ -132,10 +132,16 @@ module ActiveScaffold
 
       def active_scaffold_search_range(column, options)
         opt_value, from_value, to_value = field_search_params_range_values(column)
-        
-        select_options = active_scaffold_search_range_comparator_options(column)
-        text_field_size = ((column.column && column.column.text?) ? 15 : 10)
 
+        select_options = active_scaffold_search_range_comparator_options(column)
+        if column.column && column.column.text?
+          text_field_size = 15
+          opt_value ||= '%?%'
+        else
+          text_field_size = 10
+          opt_value ||= '='
+        end
+        
         from_value = controller.class.condition_value_for_numeric(column, from_value)
         to_value = controller.class.condition_value_for_numeric(column, to_value)
         from_value = format_number_value(from_value, column.options) if from_value.is_a?(Numeric)
