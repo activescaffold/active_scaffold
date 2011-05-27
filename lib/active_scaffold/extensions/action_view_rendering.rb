42baf5c5531243e03126ac149c0bd3bfbcdc0ce9
vhochstein <v.hochstein@highstone.de>
2011-05-28 00:01:35 +0200

    Bugfix: at least fix undefined method `render' for module `ActionView::Rendering' however do nt think that rendering will work...

--- lib/active_scaffold/extensions/action_view_rendering.rb
+++ lib/active_scaffold/extensions/action_view_rendering.rb
@@ -14,7 +14,9 @@ module ActionView
 end
 
 # wrap the action rendering for ActiveScaffold views
-module ActionView::Rendering #:nodoc:
+module ActionView #:nodoc:
+  class Renderer
+  #
   # Adds two rendering options.
   #
   # ==render :super
@@ -36,88 +38,89 @@ module ActionView::Rendering #:nodoc:
   #
   # Defining options[:label] lets you completely customize the list title for the embedded scaffold.
   #
-  def render_with_active_scaffold(*args, &block)
-    if args.first == :super
-      last_view = @view_stack.last
-      options = args[1] || {}
-      options[:locals] ||= {}
-      options[:locals].reverse_merge!(last_view[:locals] || {})
-      if last_view[:templates].nil?
-        last_view[:templates] = lookup_context.find_all_templates(last_view[:view], controller_path, !last_view[:is_template])
-        last_view[:templates].shift
-      end
-      options[:template] = last_view[:templates].shift
-      @view_stack << last_view
-      result = render_without_active_scaffold options
-      @view_stack.pop
-      result
-    elsif args.first.is_a?(Hash) and args.first[:active_scaffold]
-      require 'digest/md5'
-      options = args.first
+    def render_with_active_scaffold(*args, &block)
+      if args.first == :super
+        last_view = @view_stack.last
+        options = args[1] || {}
+        options[:locals] ||= {}
+        options[:locals].reverse_merge!(last_view[:locals] || {})
+        if last_view[:templates].nil?
+          last_view[:templates] = lookup_context.find_all_templates(last_view[:view], controller_path, !last_view[:is_template])
+          last_view[:templates].shift
+        end
+        options[:template] = last_view[:templates].shift
+        @view_stack << last_view
+        result = render_without_active_scaffold options
+        @view_stack.pop
+        result
+      elsif args.first.is_a?(Hash) and args.first[:active_scaffold]
+        require 'digest/md5'
+        options = args.first
 
-      remote_controller = options[:active_scaffold]
-      constraints = options[:constraints]
-      conditions = options[:conditions]
-      eid = Digest::MD5.hexdigest(params[:controller] + remote_controller.to_s + constraints.to_s + conditions.to_s)
-      session["as:#{eid}"] = {:constraints => constraints, :conditions => conditions, :list => {:label => args.first[:label]}}
-      options[:params] ||= {}
-      options[:params].merge! :eid => eid, :embedded => true
-      
-      id = "as_#{eid}-content"
-      url_options = {:controller => remote_controller.to_s, :action => 'index'}.merge(options[:params])
-      
-      if controller.respond_to?(:render_component_into_view)
-        controller.send(:render_component_into_view, url_options)
-      else
-        content_tag(:div, {:id => id}) do
-          url = url_for(url_options)
-          link_to(remote_controller.to_s, url, {:remote => true, :id => id}) <<
-            if ActiveScaffold.js_framework == :prototype
-            javascript_tag("new Ajax.Updater('#{id}', '#{url}', {method: 'get', evalScripts: true});")
-          elsif ActiveScaffold.js_framework == :jquery
-            javascript_tag("$('##{id}').load('#{url}');")
+        remote_controller = options[:active_scaffold]
+        constraints = options[:constraints]
+        conditions = options[:conditions]
+        eid = Digest::MD5.hexdigest(params[:controller] + remote_controller.to_s + constraints.to_s + conditions.to_s)
+        session["as:#{eid}"] = {:constraints => constraints, :conditions => conditions, :list => {:label => args.first[:label]}}
+        options[:params] ||= {}
+        options[:params].merge! :eid => eid, :embedded => true
+
+        id = "as_#{eid}-content"
+        url_options = {:controller => remote_controller.to_s, :action => 'index'}.merge(options[:params])
+
+        if controller.respond_to?(:render_component_into_view)
+          controller.send(:render_component_into_view, url_options)
+        else
+          content_tag(:div, {:id => id}) do
+            url = url_for(url_options)
+            link_to(remote_controller.to_s, url, {:remote => true, :id => id}) <<
+              if ActiveScaffold.js_framework == :prototype
+              javascript_tag("new Ajax.Updater('#{id}', '#{url}', {method: 'get', evalScripts: true});")
+            elsif ActiveScaffold.js_framework == :jquery
+              javascript_tag("$('##{id}').load('#{url}');")
+            end
           end
         end
-      end
-      
-    else
-      options = args.first
-      if options.is_a?(Hash)
-        current_view = {:view => options[:partial], :is_template => false} if options[:partial]
-        current_view = {:view => options[:template], :is_template => !!options[:template]} if current_view.nil? && options[:template]
-        current_view[:locals] = options[:locals] if !current_view.nil? && options[:locals]
-        if current_view.present?
-          @view_stack ||= []
-          @view_stack << current_view
-        end
-      end
-      result = render_without_active_scaffold(*args, &block)
-      @view_stack.pop if current_view.present?
-      result
-    end
-  end
-  alias_method_chain :render, :active_scaffold
-  
 
-  def partial_pieces(partial_path)
-    if partial_path.include?('/')
-      return File.dirname(partial_path), File.basename(partial_path)
-    else
-      return controller.class.controller_path, partial_path
+      else
+        options = args.first
+        if options.is_a?(Hash)
+          current_view = {:view => options[:partial], :is_template => false} if options[:partial]
+          current_view = {:view => options[:template], :is_template => !!options[:template]} if current_view.nil? && options[:template]
+          current_view[:locals] = options[:locals] if !current_view.nil? && options[:locals]
+          if current_view.present?
+            @view_stack ||= []
+            @view_stack << current_view
+          end
+        end
+        result = render_without_active_scaffold(*args, &block)
+        @view_stack.pop if current_view.present?
+        result
+      end
     end
-  end
-  
-  # This is the template finder logic, keep it updated with however we find stuff in rails
-  # currently this very similar to the logic in ActionBase::Base.render for options file
-  # TODO: Work with rails core team to find a better way to check for this.
-  def template_exists?(template_name, lookup_overrides = false)
-    begin
-      method = 'find_template'
-      method << '_without_active_scaffold' unless lookup_overrides
-      self.view_paths.send(method, template_name, @template_format)
-      return true
-    rescue ActionView::MissingTemplate => e
-      return false
+    alias_method_chain :render, :active_scaffold
+
+
+    def partial_pieces(partial_path)
+      if partial_path.include?('/')
+        return File.dirname(partial_path), File.basename(partial_path)
+      else
+        return controller.class.controller_path, partial_path
+      end
+    end
+
+    # This is the template finder logic, keep it updated with however we find stuff in rails
+    # currently this very similar to the logic in ActionBase::Base.render for options file
+    # TODO: Work with rails core team to find a better way to check for this.
+    def template_exists?(template_name, lookup_overrides = false)
+      begin
+        method = 'find_template'
+        method << '_without_active_scaffold' unless lookup_overrides
+        self.view_paths.send(method, template_name, @template_format)
+        return true
+      rescue ActionView::MissingTemplate => e
+        return false
+      end
     end
   end
 end
