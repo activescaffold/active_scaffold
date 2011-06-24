96027899e67f1aadc2e22214e3ee713cbad2455e
vhochstein <v.hochstein@highstone.de>
2011-06-24 19:09:59 +0200

    fix set_sorting by order_clause in case it s not a string

--- lib/active_scaffold/data_structures/sorting.rb
+++ lib/active_scaffold/data_structures/sorting.rb
@@ -128,7 +128,7 @@ module ActiveScaffold::DataStructures
 
     def set_sorting_from_order_clause(order_clause, model_table_name = nil)
       clear
-      order_clause.split(',').each do |criterion|
+      order_clause.to_s.split(',').each do |criterion|
         unless criterion.blank?
           order_parts = extract_order_parts(criterion)
           add(order_parts[:column_name], order_parts[:direction]) unless different_table?(model_table_name, order_parts[:table_name])
