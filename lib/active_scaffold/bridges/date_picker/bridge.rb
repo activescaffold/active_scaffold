879cd0544ea63377a48b4b03d291f42ff2d08faa
vhochstein <v.hochstein@highstone.de>
2011-07-02 13:23:54 +0200

    remove old copy js file code for date_picker bridge

--- lib/active_scaffold/bridges/date_picker/bridge.rb
+++ lib/active_scaffold/bridges/date_picker/bridge.rb
@@ -1,19 +1,5 @@
 ActiveScaffold::Bridges.bridge "DatePicker" do
   install do
-    directory = File.dirname(__FILE__)
-    source = File.join(directory, "public/javascripts/date_picker_bridge.js")
-    destination = File.join(Rails.root, "public/javascripts/active_scaffold/default/")
-
-    if ActiveScaffold.js_framework == :jquery
-      require File.join(directory, "lib/datepicker_bridge.rb")
-      unless defined?(ACTIVE_SCAFFOLD_INSTALL_ASSETS) && ACTIVE_SCAFFOLD_INSTALL_ASSETS == false
-        FileUtils.cp(source, destination)
-        #ActiveScaffold::Bridges::DatePickerBridge.localization(File.join(destination, 'date_picker_bridge.js'))
-      end
-    else
-      # make sure that jquery files are removed
-      FileUtils.rm(File.join(destination, 'date_picker_bridge.js')) if File.exist?(File.join(destination, 'date_picker_bridge.js'))
-    end
   end
 
   
