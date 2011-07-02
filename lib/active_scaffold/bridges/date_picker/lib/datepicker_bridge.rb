0de3b285511d1ee459f5e87af6098f67925942d8
vhochstein <v.hochstein@highstone.de>
2011-07-02 13:22:09 +0200

    add date_picker_bridge js file to asset pipeline

--- lib/active_scaffold/bridges/date_picker/lib/datepicker_bridge.rb
+++ lib/active_scaffold/bridges/date_picker/lib/datepicker_bridge.rb
@@ -48,76 +48,107 @@ module ActiveScaffold
         '%S' => 'ss'
       }
       
-      def self.localization(js_file)
-        localization = "jQuery(function($){
+      def self.localization
+        "jQuery(function($){
   if (typeof($.datepicker) === 'object') {
-    $.datepicker.regional['#{I18n.locale}'] = #{date_options.to_json};
+    #{date_options_for_locales}
     $.datepicker.setDefaults($.datepicker.regional['#{I18n.locale}']);
   }
   if (typeof($.timepicker) === 'object') {
-    $.timepicker.regional['#{I18n.locale}'] = #{datetime_options.to_json};
+    #{datetime_options_for_locales}
     $.timepicker.setDefaults($.timepicker.regional['#{I18n.locale}']);
   }
 });\n"        
-        prepend_js_file(js_file, localization)        
       end
-      
-      def self.date_options
-        date_options = I18n.translate! 'date'
-        date_picker_options = { :closeText => as_(:close),
-          :prevText => as_(:previous),
-          :nextText => as_(:next),
-          :currentText => as_(:today),
-          :monthNames => date_options[:month_names][1, (date_options[:month_names].length - 1)],
-          :monthNamesShort => date_options[:abbr_month_names][1, (date_options[:abbr_month_names].length - 1)],
-          :dayNames => date_options[:day_names],
-          :dayNamesShort => date_options[:abbr_day_names],
-          :dayNamesMin => date_options[:abbr_day_names],
-          :changeYear => true,
-          :changeMonth => true,
-        }
 
+      def self.date_options_for_locales
+        I18n.available_locales.collect do |locale|
+          locale_date_options = date_options(locale)
+          if locale_date_options
+            "$.datepicker.regional['#{locale}'] = #{locale_date_options.to_json};"
+          else
+            nil
+          end
+        end.compact.join('')
+      end
+      
+      def self.date_options(locale)
         begin
-          as_date_picker_options = I18n.translate! 'active_scaffold.date_picker_options'
-          date_picker_options.merge!(as_date_picker_options) if as_date_picker_options.is_a? Hash
-        rescue
-          Rails.logger.warn "ActiveScaffold: Missing date picker localization for your locale: #{I18n.locale}"
-        end
+          date_options = I18n.translate! 'date', :locale => locale
+          date_picker_options = { :closeText => as_(:close),
+            :prevText => as_(:previous),
+            :nextText => as_(:next),
+            :currentText => as_(:today),
+            :monthNames => date_options[:month_names][1, (date_options[:month_names].length - 1)],
+            :monthNamesShort => date_options[:abbr_month_names][1, (date_options[:abbr_month_names].length - 1)],
+            :dayNames => date_options[:day_names],
+            :dayNamesShort => date_options[:abbr_day_names],
+            :dayNamesMin => date_options[:abbr_day_names],
+            :changeYear => true,
+            :changeMonth => true,
+          }
 
-        js_format = self.to_datepicker_format(date_options[:formats][:default])
-        date_picker_options[:dateFormat] = js_format unless js_format.nil? 
-        date_picker_options
-      end
-      
-      def self.datetime_options
-        rails_time_format = I18n.translate! 'time.formats.default'
-        datetime_options = I18n.translate! 'datetime.prompts'
-        datetime_picker_options = {:ampm => false,
-          :hourText => datetime_options[:hour],
-				  :minuteText => datetime_options[:minute],
-				  :secondText => datetime_options[:second],
-        }
-        
-        begin
-          as_datetime_picker_options = I18n.translate! 'active_scaffold.datetime_picker_options'
-          datetime_picker_options.merge!(as_datetime_picker_options) if as_datetime_picker_options.is_a? Hash
+          begin
+            as_date_picker_options = I18n.translate! 'active_scaffold.date_picker_options'
+            date_picker_options.merge!(as_date_picker_options) if as_date_picker_options.is_a? Hash
+          rescue
+            Rails.logger.warn "ActiveScaffold: Missing date picker localization for your locale: #{locale}"
+          end
+
+          js_format = self.to_datepicker_format(date_options[:formats][:default])
+          date_picker_options[:dateFormat] = js_format unless js_format.nil?
+          date_picker_options
         rescue
-          Rails.logger.warn "ActiveScaffold: Missing datetime picker localization for your locale: #{I18n.locale}"
+          if locale == I18n.locale
+            raise
+          else
+            nil
+          end
         end
-        
-        date_format, time_format = self.split_datetime_format(self.to_datepicker_format(rails_time_format))
-        datetime_picker_options[:dateFormat] = date_format unless date_format.nil?
-        unless time_format.nil?
-          datetime_picker_options[:timeFormat] = time_format
-          datetime_picker_options[:ampm] = true if rails_time_format.include?('%I')
-        end
-        datetime_picker_options
+      end
+
+      def self.datetime_options_for_locales
+        I18n.available_locales.collect do |locale|
+          locale_datetime_options = datetime_options(locale)
+          if locale_datetime_options
+            "$.timepicker.regional['#{locale}'] = #{locale_datetime_options.to_json};"
+          else
+            nil
+          end
+        end.compact.join('')
       end
       
-      def self.prepend_js_file(js_file, prepend)
-        content = File.binread(js_file)
-        content.gsub!(/\A/, prepend)
-        File.open(js_file, 'wb') { |file| file.write(content) }
+      def self.datetime_options(locale)
+        begin
+          rails_time_format = I18n.translate! 'time.formats.default', :locale => locale
+          datetime_options = I18n.translate! 'datetime.prompts', :locale => locale
+          datetime_picker_options = {:ampm => false,
+            :hourText => datetime_options[:hour],
+            :minuteText => datetime_options[:minute],
+            :secondText => datetime_options[:second],
+          }
+
+          begin
+            as_datetime_picker_options = I18n.translate! 'active_scaffold.datetime_picker_options'
+            datetime_picker_options.merge!(as_datetime_picker_options) if as_datetime_picker_options.is_a? Hash
+          rescue
+            Rails.logger.warn "ActiveScaffold: Missing datetime picker localization for your locale: #{locale}"
+          end
+
+          date_format, time_format = self.split_datetime_format(self.to_datepicker_format(rails_time_format))
+          datetime_picker_options[:dateFormat] = date_format unless date_format.nil?
+          unless time_format.nil?
+            datetime_picker_options[:timeFormat] = time_format
+            datetime_picker_options[:ampm] = true if rails_time_format.include?('%I')
+          end
+          datetime_picker_options
+        rescue
+          if locale == I18n.locale
+            raise
+          else
+            nil
+          end
+        end
       end
       
       def self.to_datepicker_format(rails_format)
