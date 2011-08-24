2e2769fc3a8e7e2220c0449ac2aef3afff8a65d2
scambra <sergio@entrecables.com>
2011-08-24 14:24:37 +0200

    Add dragonfly bridge

---
+++ lib/active_scaffold/bridges/dragonfly/lib/dragonfly_bridge_helpers.rb
@@ -0,0 +1,12 @@
+module ActiveScaffold
+  module Bridges
+    module Dragonfly
+      module Lib
+        module DragonflyBridgeHelpers
+          mattr_accessor :thumbnail_style
+          self.thumbnail_style = 'x30>'
+        end
+      end
+    end
+  end
+end
