34e6de4b85e9ac711bf8190dbe6e1372e13d3f39
scambra <sergio@entrecables.com>
2011-08-30 09:40:42 +0200

    fix update columns when create and update columns are different

--- lib/active_scaffold/actions/core.rb
+++ lib/active_scaffold/actions/core.rb
@@ -40,7 +40,7 @@ module ActiveScaffold::Actions
           else
             params[:record]
           end
-          @record = update_record_from_params(@record, active_scaffold_config.update.columns, hash)
+          @record = update_record_from_params(@record, active_scaffold_config.send(params[:id] ? :update : :create).columns, hash)
         else
           value = column_value_from_param_value(@record, column, params[:value])
           @record.send "#{column.name}=", value
