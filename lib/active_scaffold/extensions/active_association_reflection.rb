c42ab09dad49bd54ff60ea5e36300f42409fc28f
vhochstein <v.hochstein@highstone.de>
2011-09-01 20:36:22 +0200

    Bugfix: deprecation warning build_assocation has been redefined

--- lib/active_scaffold/extensions/active_association_reflection.rb
+++ lib/active_scaffold/extensions/active_association_reflection.rb
@@ -1,13 +1,13 @@
 # Bugfix: building an sti model from an association fails
 # https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/6306-collection-associations-build-method-not-supported-for-sti
 ActiveRecord::Reflection::AssociationReflection.class_eval do
-  def build_association(*opts)
+  def build_association(*opts, &block)
     col = klass.inheritance_column.to_sym
     if !col.nil? && opts.first.is_a?(Hash) && (opts.first.symbolize_keys[col])
       sti_model = opts.first.delete(col)
-      sti_model.to_s.camelize.constantize.new(*opts)
+      sti_model.to_s.camelize.constantize.new(*opts, &block)
     else
-      klass.new(*opts)
+      klass.new(*opts, &block)
     end
   end
 end
