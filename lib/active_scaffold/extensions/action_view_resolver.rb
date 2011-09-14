5af201110d33875feb89781d5cc1b30c1efa0e26
scambra <sergio@entrecables.com>
2011-09-14 15:12:31 +0200

    simplify template lookup and fix it for rails 3.1

--- lib/active_scaffold/extensions/action_view_resolver.rb
+++
@@ -1,9 +0,0 @@
-module ActionView
-  class ActiveScaffoldResolver < FileSystemResolver
-    # standard resolvers have a base path to views and append a controller subdirectory
-    # activescaffolds view path do not have a subdir, so just remove the prefix
-    def find_templates(name, prefix, partial, details)
-      super(name,'',partial, details)
-    end
-  end
-end
