8444b4bc6f6abfbbca46b2711b8fb487f5016217
scambra <sergio@entrecables.com>
2011-09-15 11:21:53 +0200

    fix record select js framework

--- lib/active_scaffold/bridges/record_select.rb
+++ lib/active_scaffold/bridges/record_select.rb
@@ -1,6 +1,5 @@
 class ActiveScaffold::Bridges::RecordSelect < ActiveScaffold::DataStructures::Bridge
   def self.install
-    RecordSelect::Config.js_framework = ActiveScaffold.js_framework
     require File.join(File.dirname(__FILE__), "record_select/helpers.rb")
   end
   def self.stylesheets
@@ -10,3 +9,4 @@ class ActiveScaffold::Bridges::RecordSelect < ActiveScaffold::DataStructures::Br
     'record_select'
   end
 end
+RecordSelect::Config.js_framework = ActiveScaffold.js_framework
