8fd49e7ef89a21c5515e0c19b5d7d6a783806517
scambra <sergio@entrecables.com>
2011-09-15 11:54:01 +0200

    autodetect js framework

--- install.rb
+++
@@ -1,3 +0,0 @@
-File.open(File.expand_path('../../../../config/initializers/active_scaffold.rb', __FILE__), 'w') do |f|
-  f << "#ActiveSupport.on_load(:active_scaffold) { self.js_framework = :jquery }\n"
-end
