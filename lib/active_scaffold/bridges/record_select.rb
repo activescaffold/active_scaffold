235bc6a18541a7a5a5a44555524aaaccf8000022
scambra <sergio@entrecables.com>
2011-09-15 11:18:48 +0200

    support to ActiveScaffold plugins add their stylesheets and javascripts

--- lib/active_scaffold/bridges/record_select.rb
+++ lib/active_scaffold/bridges/record_select.rb
@@ -1,5 +1,12 @@
 class ActiveScaffold::Bridges::RecordSelect < ActiveScaffold::DataStructures::Bridge
   def self.install
+    RecordSelect::Config.js_framework = ActiveScaffold.js_framework
     require File.join(File.dirname(__FILE__), "record_select/helpers.rb")
+  end
+  def self.stylesheets
+    'record_select'
+  end
+  def self.javascripts
+    'record_select'
   end
 end
