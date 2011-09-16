b30a440bccc885690bc7852cbea0d66e299a4396
scambra <sergio@entrecables.com>
2011-09-16 15:35:29 +0200

    pass block in build_association

--- lib/active_scaffold/extensions/active_association_reflection.rb
+++ lib/active_scaffold/extensions/active_association_reflection.rb
@@ -1,13 +1,22 @@
 # Bugfix: building an sti model from an association fails
 # https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/6306-collection-associations-build-method-not-supported-for-sti
+# https://github.com/rails/rails/issues/815
+# https://github.com/rails/rails/pull/1686
 ActiveRecord::Reflection::AssociationReflection.class_eval do
-  def build_association(*opts)
-    col = klass.inheritance_column.to_sym
-    if !col.nil? && opts.first.is_a?(Hash) && (opts.first.symbolize_keys[col])
-      sti_model = opts.first.delete(col)
-      sti_model.to_s.camelize.constantize.new(*opts)
+  def klass_with_sti(*opts)
+    sti_col = klass.inheritance_column
+    if (h = opts.first).is_a? Hash and (passed_type = ( h[sti_col] || h[sti_col.to_sym] )) and (new_klass = active_record.send(:compute_type, passed_type)) < klass
+      new_klass
     else
-      klass.new(*opts)
+      klass
     end
+  end
+  def build_association(*opts, &block)
+    self.original_build_association_called = true
+    klass_with_sti(*opts).new(*opts, &block)
+  end
+  def create_association(*opts, &block)
+    self.original_build_association_called = true
+    klass_with_sti(*opts).create(*opts, &block)
   end
 end
