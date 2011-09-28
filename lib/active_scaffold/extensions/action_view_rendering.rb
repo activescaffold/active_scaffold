09b1d2fa9cd2fdc7a576e213e07869b569977afe
scambra <sergio@entrecables.com>
2011-09-28 13:22:49 +0200

    fix duplicated in embedded rendering

--- lib/active_scaffold/extensions/action_view_rendering.rb
+++ lib/active_scaffold/extensions/action_view_rendering.rb
@@ -64,16 +64,18 @@ module ActionView::Helpers #:nodoc:
         options[:params] ||= {}
         options[:params].merge! :eid => eid, :embedded => true
 
-        id = "as_#{eid}-content"
+        id = "as_#{eid}-embedded"
         url_options = {:controller => remote_controller.to_s, :action => 'index'}.merge(options[:params])
 
         if controller.respond_to?(:render_component_into_view)
           controller.send(:render_component_into_view, url_options)
         else
-          content_tag(:div, {:id => id}) do
+          content_tag(:div, :id => id, :class => 'active-scaffold-component') do
             url = url_for(url_options)
-            link_to(remote_controller.to_s, url, {:remote => true, :id => id}) <<
-              if ActiveScaffold.js_framework == :prototype
+            content_tag(:div, :class => 'active-scaffold-header') do
+              content_tag :h2, link_to(args.first[:label] || active_scaffold_config_for(remote_controller.to_s.singularize).list.label, url, :remote => true)
+            end <<
+            if ActiveScaffold.js_framework == :prototype
               javascript_tag("new Ajax.Updater('#{id}', '#{url}', {method: 'get', evalScripts: true});")
             elsif ActiveScaffold.js_framework == :jquery
               javascript_tag("$('##{id}').load('#{url}');")
