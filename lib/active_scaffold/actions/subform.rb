69bd07bf9f4ae8015f38eaca6abee628331ba706
scambra <sergio@entrecables.com>
2011-09-29 14:53:03 +0200

    dry last commit

--- lib/active_scaffold/actions/subform.rb
+++ lib/active_scaffold/actions/subform.rb
@@ -13,17 +13,7 @@ module ActiveScaffold::Actions
 
       # NOTE: we don't check whether the user is allowed to update this record, because if not, we'll still let them associate the record. we'll just refuse to do more than associate, is all.
       @record = @column.association.klass.find(params[:associated_id]) if params[:associated_id]
-      @record ||= @column.singular_association? ? @parent_record.send("build_#{@column.name}".to_sym) : @parent_record.send(@column.name).build
-      reflection = @parent_record.class.reflect_on_association(@column.name)
-      if reflection && reflection.reverse && @parent_record.new_record?
-        reverse_macro = @record.class.reflect_on_association(reflection.reverse).macro
-        if [:has_one, :belongs_to].include?(reverse_macro) # singular
-          @record.send(:"#{reflection.reverse}=", @parent_record)
-        # TODO: Might want to extend with this branch in the future
-        # else # plural
-        #   @record.send(:"#{reflection.reverse}") << @parent_record
-        end
-      end
+      @record ||= build_associated(@column, @parent_record)
 
       @scope = "[#{@column.name}]"
       @scope += (@record.new_record?) ? "[#{(Time.now.to_f*1000).to_i.to_s}]" : "[#{@record.id}]" if @column.plural_association?
