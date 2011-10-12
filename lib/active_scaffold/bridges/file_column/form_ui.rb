1cf4aefe793f1cdbbd4803fdd11880342586e06f
eric.beland@nemoves.com <eric.beland@nemoves.com>
2011-10-12 15:50:07 -0400

    File column bridge tweaks

--- lib/active_scaffold/bridges/file_column/form_ui.rb
+++ lib/active_scaffold/bridges/file_column/form_ui.rb
@@ -3,35 +3,32 @@ module ActiveScaffold
     # Helpers that assist with the rendering of a Form Column
     module FormColumnHelpers
       def active_scaffold_input_file_column(column, options)
-        if @record.send(column.name) 
-          # we already have a value?  display the form for deletion.
+       if @record.send(column.name) 
+          # we already have a value? display the form for deletion.
           if ActiveScaffold.js_framework == :jquery
-            js_remove_file_code = "$(this).prev().val('true'); $(this).parent().hide().next().show(); return false;";
+            remove_file_js = "$(this).prev().val('true'); $(this).parent().hide().next().show(); return false;";
           else
-            js_remove_file_code = "$(this).previous().value='true'; p=$(this).up(); p.hide(); p.next().show(); return false;";
+            remove_file_js = "$(this).previous().value='true'; p=$(this).up(); p.hide(); p.next().show(); return false;";
+          end            
+          
+          hidden_options = options.dup
+          hidden_options[:id] += '_delete'
+          hidden_options[:name].sub!("[#{column.name}]", "[delete_#{column.name}]")
+          hidden_options[:value] = 'false'          
+          custom_hidden_field_tag = hidden_field(:record, column.name, hidden_options)
+
+          content_tag(:div) do
+            content_tag(:div) do
+              content = get_column_value(@record, column) + " #{custom_hidden_field_tag} | "
+              content += content_tag(:a, as_(:remove_file), {:href => '#', :onclick => remove_file_js})
+              content += content_tag(:div, file_column_field("record", column.name, options), :style => "display: none")
+            end           
           end
-          content_tag(
-            :div, 
-            content_tag(
-              :div, 
-              get_column_value(@record, column) + " " +
-              custom_hidden_field_tag +
-              " | " +
-                content_tag(:a, as_(:remove_file), {:href => '#', :onclick => js_remove_file_code}),
-              {}
-            ) +
-            content_tag(
-              :div,
-              file_column_field("record", column.name, options),
-              :style => "display: none"
-            ),
-            {}
-          )
-        else
-          # no, just display the file_column_field
-          file_column_field("record", column.name, options)
-        end
-      end      
+       else
+          file_column_field("record", column.name, options)          
+       end        
+      end
+      
     end
   end
 end
