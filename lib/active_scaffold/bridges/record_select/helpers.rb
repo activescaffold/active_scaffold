6266e6d5f475cee1fe57caae2b9496bef14db13e
scambra <sergio@entrecables.com>
2011-12-21 12:01:47 +0100

    fix record_select_autocomplete

--- lib/active_scaffold/bridges/record_select/helpers.rb
+++ lib/active_scaffold/bridges/record_select/helpers.rb
@@ -49,7 +49,7 @@ class ActiveScaffold::Bridges::RecordSelect
       
       def active_scaffold_record_select_autocomplete(column, options)
         record_select_options = active_scaffold_input_text_options(options).merge(
-          :controller => remote_controller
+          :controller => active_scaffold_controller_for(@record.class).controller_path
         )
         html = record_select_autocomplete(options[:name], @record, record_select_options)
         html = self.class.field_error_proc.call(html, self) if @record.errors[column.name].any?
