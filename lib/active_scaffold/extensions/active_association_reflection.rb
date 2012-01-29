364ad036c2b572a7d46391fa93c8c48ccb450c0d
Nick Rogers <nick@Nicks-MacBook-Air.local>
2012-01-29 12:44:06 -0500

    Fix STI reflection patch to work with rails 3.2

--- lib/active_scaffold/extensions/active_association_reflection.rb
+++ lib/active_scaffold/extensions/active_association_reflection.rb
@@ -2,6 +2,7 @@
 # https://rails.lighthouseapp.com/projects/8994-ruby-on-rails/tickets/6306-collection-associations-build-method-not-supported-for-sti
 # https://github.com/rails/rails/issues/815
 # https://github.com/rails/rails/pull/1686
+# Does not appear to be resolved under rails 3.2 as the above issues were closed without resolution in the official rails tree.
 ActiveRecord::Reflection::AssociationReflection.class_eval do
   def klass_with_sti(*opts)
     sti_col = klass.inheritance_column
@@ -12,11 +13,9 @@ ActiveRecord::Reflection::AssociationReflection.class_eval do
     end
   end
   def build_association(*opts, &block)
-    self.original_build_association_called = true
     klass_with_sti(*opts).new(*opts, &block)
   end
   def create_association(*opts, &block)
-    self.original_build_association_called = true
     klass_with_sti(*opts).create(*opts, &block)
   end
 end
