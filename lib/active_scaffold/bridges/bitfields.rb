eec5640f200185d33ecacc26d230b1e4c8b1e124
scambra <sergio@entrecables.com>
2012-02-22 15:50:44 +0100

    bitfields bridge

---
+++ lib/active_scaffold/bridges/bitfields.rb
@@ -0,0 +1,6 @@
+class ActiveScaffold::Bridges::Bitfields < ActiveScaffold::DataStructures::Bridge
+  def self.install
+    require File.join(File.dirname(__FILE__), "bitfields/bitfields_bridge")
+    ActiveScaffold::Config::Core.send :include, ActiveScaffold::Bridges::Bitfields::BitfieldsBridge
+  end
+end
