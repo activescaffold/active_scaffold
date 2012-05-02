b6e1964eba8bc3921931f1bf315cda217404dd29
scambra <sergio@entrecables.com>
2012-05-02 14:25:57 +0200

    add outer window option for page links

--- lib/active_scaffold/helpers/pagination_helpers.rb
+++ lib/active_scaffold/helpers/pagination_helpers.rb
@@ -5,9 +5,9 @@ module ActiveScaffold
         link_to page_number, url_options.merge(:page => page_number), options.merge(:class => "as_paginate")
       end
 
-      def pagination_ajax_links(current_page, url_options, options, window_size)
-        start_number = current_page.number - window_size
-        end_number = current_page.number + window_size
+      def pagination_ajax_links(current_page, url_options, options, inner_window, outer_window)
+        start_number = current_page.number - inner_window
+        end_number = current_page.number + inner_window
         start_number = 1 if start_number <= 0
         if current_page.pager.infinite?
           offsets = [20, 100]
@@ -16,23 +16,24 @@ module ActiveScaffold
         end
 
         html = []
-        unless start_number == 1
-          last_page = 1
-          html << pagination_ajax_link(last_page, url_options, options)
-          if current_page.pager.infinite?
-            offsets.reverse.each do |offset|
-              page = current_page.number - offset
-              if page < start_number && page > 1
-                html << '..' if page > last_page + 1
-                html << pagination_ajax_link(page, params)
-                last_page = page
-              end
+        last_page = 1
+        last_page.upto(last_page + outer_window) do |num|
+          html << pagination_ajax_link(num, url_options, options)
+          last_page = num
+        end
+        if current_page.pager.infinite?
+          offsets.reverse.each do |offset|
+            page = current_page.number - offset
+            if page < start_number && page > last_page
+              html << '..' if page > last_page + 1
+              html << pagination_ajax_link(page, params)
+              last_page = page
             end
           end
-          html << ".." if start_number > last_page + 1
         end
+        html << ".." if start_number > last_page + 1
 
-        start_number.upto(end_number) do |num|
+        [start_number, last_page + 1].max.upto(end_number) do |num|
           if current_page.number == num
             html << content_tag(:span, num.to_s, {:class => "as_paginate current"})
           else
@@ -45,8 +46,10 @@ module ActiveScaffold
             html << '..' << pagination_ajax_link(current_page.number + offset, url_options, options)
           end
         else
-          html << ".." unless end_number >= current_page.pager.last.number - 1
-          html << pagination_ajax_link(current_page.pager.last.number, url_options, options) unless end_number == current_page.pager.last.number
+          html << ".." unless end_number >= current_page.pager.last.number - outer_window - 1
+          [end_number + 1, current_page.pager.last.number - outer_window].max.upto(current_page.pager.last.number) do |num|
+            html << pagination_ajax_link(num, url_options, options)
+          end
         end
         html.join(' ').html_safe
       end
