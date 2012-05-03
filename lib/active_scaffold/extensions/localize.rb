fc803381046400ce5f0ff6541853493ba4795179
scambra <sergio@entrecables.com>
2012-05-03 13:13:45 +0200

    allow to override some human condition strings in i18n

--- lib/active_scaffold/extensions/localize.rb
+++ lib/active_scaffold/extensions/localize.rb
@@ -1,7 +1,7 @@
 class Object
   def as_(key, options = {})
     unless key.blank?
-      text = I18n.translate "#{key}", {:scope => [:active_scaffold], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
+      text = I18n.translate "#{key}", {:scope => [:active_scaffold, *options.delete(:scope)], :default => key.is_a?(String) ? key : key.to_s.titleize}.merge(options)
       # text = nil if text.include?('translation missing:')
     end
     text ||= key 
