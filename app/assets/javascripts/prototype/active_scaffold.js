8003e439daf8cfba5ffcf5c1098594436f01ccb7
scambra <sergio@entrecables.com>
2012-07-09 14:57:46 +0200

    fix reset link in search and field search

--- app/assets/javascripts/prototype/active_scaffold.js
+++ app/assets/javascripts/prototype/active_scaffold.js
@@ -95,7 +95,7 @@ document.observe("dom:loaded", function() {
     var action_link = ActiveScaffold.find_action_link(as_cancel);
     
     if (action_link) {
-      var refresh_data = action_link.readAttribute('data-cancel-refresh');
+      var refresh_data = action_link.readAttribute('data-cancel-refresh') || as_cancel.readAttribute('data-refresh');
       if (refresh_data && action_link.refresh_url) {
         event.memo.url = action_link.refresh_url;
       } else if (!refresh_data || as_cancel.readAttribute('href').blank()) {
