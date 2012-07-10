c6cb02e102b9b1da9700a26a4f7a174873d69626
scambra <sergio@entrecables.com>
2012-07-10 14:13:37 +0200

    drop rails 3.1 support

--- lib/active_scaffold/extensions/action_controller_rescueing.rb
+++ lib/active_scaffold/extensions/action_controller_rescueing.rb
@@ -1,5 +1,6 @@
 module ActionController #:nodoc:
   class Base
+    # adding to ActionController::Base so it can overrided in ApplicationController
     def deny_access
       head :unauthorized
     end
