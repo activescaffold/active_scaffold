2e1f4281c1a530b8c34aae4472a1414878aaab7d
scambra <sergio@entrecables.com>
2012-07-20 08:37:44 +0200

    move chosen JS to a file included only when using chosen

--- app/assets/javascripts/jquery/active_scaffold.js
+++ app/assets/javascripts/jquery/active_scaffold.js
@@ -214,16 +214,6 @@ jQuery(document).ready(function() {
     e.preventDefault();
   });
   
-  jQuery(document).on('as:element_updated', function(event) {
-    jQuery('select.chosen', event.target).chosen();
-  });
-  jQuery(document).on('as:action_success', 'a.as_action', function(event, action_link) {
-    if (action_link.adapter) {
-      jQuery('select.chosen', action_link.adapter).chosen();
-    }
-  });
-  jQuery('select.chosen').chosen();
-
 });
 
 /* Simple Inheritance
