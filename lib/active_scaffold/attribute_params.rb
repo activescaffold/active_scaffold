c4a685dc0b478bab44fc6bfaf2c436fad004e04b
Sergio Cambra <sergio@programatica.es>
2013-04-05 14:28:58 +0200

    fix detection of empty for has_and_belongs_to_many on subforms, fixes #258

--- lib/active_scaffold/attribute_params.rb
+++ lib/active_scaffold/attribute_params.rb
@@ -217,7 +217,7 @@ module ActiveScaffold
         if value.is_a?(Hash)
           attributes_hash_is_empty?(value, klass)
         elsif value.is_a?(Array)
-          value.any? {|id| id.respond_to?(:empty?) ? !id.empty? : true}
+          value.all?(&:blank?)
         else
           value.respond_to?(:empty?) ? value.empty? : false
         end
