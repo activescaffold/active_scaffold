adedcfaf7e116fbbd5ce66c0bc34175956c3e1b0
Sergio Cambra <sergio@programatica.es>
2013-06-10 16:03:29 -1000

    Use list_columns method for setting associations includes, so columns excluded with config_list plugin are not used

--- lib/active_scaffold/actions/list.rb
+++ lib/active_scaffold/actions/list.rb
@@ -61,7 +61,12 @@ module ActiveScaffold::Actions
     # The actual algorithm to prepare for the list view
     def set_includes_for_columns(action = :list)
       @cache_associations = true
-      includes_for_list_columns = active_scaffold_config.send(action).columns.collect_visible(:flatten => true){ |c| c.includes }.flatten.uniq.compact
+      columns = if respond_to?(:"#{action}_columns")
+        send(:"#{action}_columns")
+      else
+        active_scaffold_config.send(action).columns.collect_visible(:flatten => true)
+      end
+      includes_for_list_columns = columns.map{ |c| c.includes }.flatten.uniq.compact
       self.active_scaffold_includes.concat includes_for_list_columns
     end
 
