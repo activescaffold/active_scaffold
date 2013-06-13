d2e9ebf81fc33ca97ee6649247f26b93a68c3372
Sergio Cambra <sergio@programatica.es>
2013-06-13 07:34:17 +0200

    add record to search_column_label, it can be useful for using I18n.t

--- lib/active_scaffold/helpers/search_column_helpers.rb
+++ lib/active_scaffold/helpers/search_column_helpers.rb
@@ -54,7 +54,7 @@ module ActiveScaffold
       def search_attribute(column, record)
         column_options = active_scaffold_search_options(column).merge(:object => record)
         field = active_scaffold_search_for column, column_options
-        %|<dl><dt>#{label_tag search_label_for(column, column_options), search_column_label(column)}</dt><dd>#{field}</dd></dl>|.html_safe
+        %|<dl><dt>#{label_tag search_label_for(column, column_options), search_column_label(column, record)}</dt><dd>#{field}</dd></dl>|.html_safe
       end
 
       def search_label_for(column, options)
@@ -233,8 +233,8 @@ module ActiveScaffold
       ##
       ## Search column override signatures
       ##
-          
-      def search_column_label(column)
+      
+      def search_column_label(column, record)
         column.label
       end
 
