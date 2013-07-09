f77e3116f31e5aee2f7362b4334959e64e97c1a3
Sergio Cambra <sergio@programatica.es>
2013-07-09 07:52:59 -1000

    prepare mock_app for testing

---
+++ test/mock_app/config/application.rb
@@ -0,0 +1,11 @@
+require File.expand_path('../boot', __FILE__)
+
+require "rails/all"
+require "rails/test_unit/railtie"
+
+module RailsApp
+  class Application < Rails::Application
+    config.filter_parameters << :password
+    config.action_mailer.default_url_options = { :host => "localhost:3000" }
+  end
+end
