c4036937e173a1c8c01b90ce86fdde23b9d57ba7
Sergio Cambra <sergio@programatica.es>
2013-07-12 13:43:00 +0200

    Revert "it's possible to sort using a function, in that case ActiveScaffold can't get the column"
    This reverts commit d939b1d40cff5cb82f28ec84433ccd2d61506edc.

--- lib/active_scaffold/data_structures/sorting.rb
+++ lib/active_scaffold/data_structures/sorting.rb
@@ -35,8 +35,9 @@ module ActiveScaffold::DataStructures
       direction ||= 'ASC'
       direction = direction.to_s.upcase
       column = get_column(column_name)
+      raise ArgumentError, "Could not find column #{column_name}" if column.nil?
       raise ArgumentError, "Sorting direction unknown" unless [:ASC, :DESC].include? direction.to_sym
-      @clauses << [column, direction.untaint] if column and column.sortable?
+      @clauses << [column, direction.untaint] if column.sortable?
       raise ArgumentError, "Can't mix :method- and :sql-based sorting" if mixed_sorting?
     end
 
