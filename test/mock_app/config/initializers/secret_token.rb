202282e5157336ee13f51ac3048c2ed359676641
Sergio Cambra <sergio@programatica.es>
2013-08-08 12:44:46 +0200

    update scope syntax to rails4

--- test/mock_app/config/initializers/secret_token.rb
+++ test/mock_app/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 RailsApp::Application.config.secret_token = 'e997edf9d7eba5cf89a76a046fa53f5d66261d22cfcf29e3f538c75ad2d175b106bd5d099f44f6ce34ad3b3162d71cfaa37d2d4f4b38645288331427b4c2a607'
+RailsApp::Application.config.secret_key_base = 'b1acda2299bae3ea6c5fb3cf4ac75ec552b952a2dd925873f1662091bb83e07afd0ad0cc60af94d621554da3d44ceb44bb4ed2279e536a3dac88d5e75662bda2'
