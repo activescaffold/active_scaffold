92995cff4911202f0101170a139a0155990c9cac
Nikolay Kondratyev <nkondratyev@yandex.ru>
2013-08-13 20:53:12 +0600

    Require jquery/date_picker_bridge asset unless date_picker bridge was excluded

--- lib/active_scaffold/bridges/date_picker.rb
+++ lib/active_scaffold/bridges/date_picker.rb
@@ -1,12 +1,15 @@
 module ActiveScaffold::Bridges
   class DatePicker < ActiveScaffold::DataStructures::Bridge
     autoload :Helper, 'active_scaffold/bridges/date_picker/helper'
+
     def self.install
       require File.join(File.dirname(__FILE__), "date_picker/ext.rb")
     end
+
     def self.install?
       ActiveScaffold.js_framework == :jquery
     end
+
     def self.localization
       "jQuery(function($){
   if (typeof($.datepicker) === 'object') {
@@ -18,6 +21,10 @@ module ActiveScaffold::Bridges
     $.timepicker.setDefaults($.timepicker.regional['#{::I18n.locale}']);
   }
 });\n"        
+    end
+
+    def self.javascripts
+      ['jquery/date_picker_bridge']
     end
   end
 end
