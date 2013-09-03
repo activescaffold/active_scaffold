aa5e771760ad1e30f38394a251c70d67c3eee20e
Sergio Cambra <sergio@programatica.es>
2013-09-03 10:31:40 +0200

    change version to 3.4.0.rc

--- lib/active_scaffold/version.rb
+++ lib/active_scaffold/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffold
   module Version
     MAJOR = 3
-    MINOR = 3
-    PATCH = "4.rc"
+    MINOR = 4
+    PATCH = "0.rc"
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
