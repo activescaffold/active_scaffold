f2df12b459bc4ce462a1f76909234fabcf45729e
Sergio Cambra <sergio@programatica.es>
2013-10-07 19:43:32 -1000

    detect association order in nested scaffolds when using rails4

--- lib/active_scaffold/data_structures/nested_info.rb
+++ lib/active_scaffold/data_structures/nested_info.rb
@@ -119,7 +119,11 @@ module ActiveScaffold::DataStructures
     end
 
     def default_sorting
-      association.options[:order]
+      if association.options[:order] # TODO remove when rails 3 compatibility is removed
+        association.options[:order]
+      elsif association.respond_to?(:scope) # rails 4
+        association.klass.class_eval(&association.scope).values[:order] if association.scope.is_a? Proc
+      end
     end
     
     def to_params
