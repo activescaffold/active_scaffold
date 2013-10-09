66dec4bde1d1a7daaa0f288ff8686acd1e63995b
Sergio Cambra <sergio@programatica.es>
2013-10-09 04:02:55 +0200

    Clean how AS is loaded

--- lib/active_scaffold/engine.rb
+++ lib/active_scaffold/engine.rb
@@ -1,4 +1,25 @@
 module ActiveScaffold
   class Engine < ::Rails::Engine
+    initializer "active_scaffold.action_controller" do |app|
+      ActiveSupport.on_load :action_controller do
+        include ActiveScaffold::Core
+        include ActiveScaffold::RespondsToParent
+        include ActiveScaffold::Helpers::ControllerHelpers
+        include ActiveRecordPermissions::ModelUserAccess::Controller
+      end
+    end
+
+    initializer "active_scaffold.action_view" do |app|
+      ActiveSupport.on_load :action_view do
+        include ActiveScaffold::Helpers::ViewHelpers
+      end
+    end
+
+    initializer "active_scaffold.active_record" do |app|
+      ActiveSupport.on_load :active_record do
+        include ActiveRecordPermissions::ModelUserAccess::Model
+      end
+    end
+
   end
 end
