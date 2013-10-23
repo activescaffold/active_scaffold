d17c02eb9f46d717bb7835e88a7a13bfd52bb928
Sergio Cambra <sergio@programatica.es>
2013-10-22 23:59:45 -1000

    display number of items when pagination is disabled, fixes #298

--- lib/active_scaffold/extensions/paginator_extensions.rb
+++ lib/active_scaffold/extensions/paginator_extensions.rb
@@ -4,13 +4,17 @@ class Paginator
 
   # Total number of pages
   def number_of_pages_with_infinite
-    number_of_pages_without_infinite unless infinite?
+    number_of_pages_without_infinite if @count
   end
   alias_method_chain :number_of_pages, :infinite
   
   # Is this an "infinite" paginator
   def infinite?
     @count.nil?
+  end
+
+  def count
+    @count || first.items.size
   end
   
   class Page
