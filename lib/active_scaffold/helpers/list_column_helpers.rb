70fac9dc4b1c2d6b1ba3a84ee6afb018c044be2f
Sergio Cambra <sergio@programatica.es>
2014-05-05 21:58:36 -1000

    unset id from inplace edit pattern, so create form on self-nested association has no duplicated id

--- lib/active_scaffold/helpers/list_column_helpers.rb
+++ lib/active_scaffold/helpers/list_column_helpers.rb
@@ -235,6 +235,8 @@ module ActiveScaffold
           column.form_ui = :select if (column.association && column.form_ui.nil?)
           options = active_scaffold_input_options(column).merge(:object => new_model)
           options[:class] = "#{options[:class]} inplace_field"
+          options[:"data-id"] = options[:id]
+          options[:id] = nil
           content_tag(:div, active_scaffold_input_for(column, nil, options), :style => "display:none;", :class => inplace_edit_control_css_class).tap do
             @record = old_record # TODO remove when relying on @record is removed
           end
