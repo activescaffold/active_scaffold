a188e73054033b82ef9b5f997a8c4c645e4f955d
Sergio Cambra <sergio@programatica.es>
2014-05-06 13:56:17 +0200

    fix calculate query for rails 4.1

---
+++ test/misc/calculation_test.rb
@@ -0,0 +1,37 @@
+require 'test_helper'
+
+class ClassWithFinder
+  include ActiveScaffold::Finder
+  def conditions_for_collection; end
+  def conditions_from_params; end
+  def conditions_from_constraints; end
+  def joins_for_collection; end
+  def custom_finder_options
+    {}
+  end
+  def beginning_of_chain
+    active_scaffold_config.model
+  end
+  def conditional_get_support?; end
+  def params; {}; end
+end
+
+class CalculationTest < Test::Unit::TestCase
+  def setup
+    @klass = ClassWithFinder.new
+    @klass.stubs(:active_scaffold_config).returns(mock { stubs(:model).returns(Building) })
+    @klass.stubs(:active_scaffold_session_storage).returns({})
+  end
+
+  def test_calculation_with_conditions
+    @klass.expects(:conditions_for_collection).returns(['"buildings"."name" LIKE ? OR "people"."first_name" LIKE ?', '%foo%', '%foo%'])
+    @klass.expects(:active_scaffold_references).returns([:owner])
+    @klass.active_scaffold_config.expects(:list).returns(mock { stubs(:count_includes).returns(nil) })
+
+    column = mock { stubs(:field).returns('"buildings"."id"') }
+    @klass.active_scaffold_config.expects(:columns).returns(mock { stubs(:"[]").returns(column) })
+    query = @klass.send :calculate_query
+    
+    assert_nothing_raised { query.count }
+  end
+end
