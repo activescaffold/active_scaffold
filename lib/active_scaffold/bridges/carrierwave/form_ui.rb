9e2e7b622bfe9a57fcf3e6baf2e93f3620dfc64b
Sergio Cambra <sergio@programatica.es>
2014-05-28 12:04:02 +0200

    remove use of @record on bridges

--- lib/active_scaffold/bridges/carrierwave/form_ui.rb
+++ lib/active_scaffold/bridges/carrierwave/form_ui.rb
@@ -2,8 +2,12 @@ module ActiveScaffold
   module Helpers
     module FormColumnHelpers
       def active_scaffold_input_carrierwave(column, options)
+        record = options[:object]
+        ActiveSupport::Deprecation.warn "Relying on @record is deprecated, include :object in html_options with record.", caller if record.nil? # TODO Remove when relying on @record is removed
+        record ||= @record # TODO Remove when relying on @record is removed
         options = active_scaffold_input_text_options(options.merge(column.options))
-        carrierwave = @record.send("#{column.name}")
+
+        carrierwave = record.send("#{column.name}")
         if !carrierwave.file.blank?
 
           remove_field_options = {
@@ -29,7 +33,7 @@ module ActiveScaffold
           input = file_field(:record, column.name, options.merge(:onchange => js_dont_remove_file_code))
           content_tag( :div,
             content_tag(:div, (
-                get_column_value(@record, column) + " | " +
+                get_column_value(record, column) + " | " +
                   hidden_field(:record, "#{column.name}_cache", cache_field_options) +
                   hidden_field(:record, "remove_#{column.name}", remove_field_options) +
                   content_tag(:a, as_(:remove_file), {:href => '#', :onclick => js_remove_file_code})
--- lib/active_scaffold/bridges/file_column/form_ui.rb
+++ lib/active_scaffold/bridges/file_column/form_ui.rb
@@ -3,7 +3,10 @@ module ActiveScaffold
     # Helpers that assist with the rendering of a Form Column
     module FormColumnHelpers
       def active_scaffold_input_file_column(column, options)
-       if @record.send(column.name) 
+        record = options[:object]
+        ActiveSupport::Deprecation.warn "Relying on @record is deprecated, include :object in html_options with record.", caller if record.nil? # TODO Remove when relying on @record is removed
+        record ||= @record # TODO Remove when relying on @record is removed
+        if record.send(column.name) 
           # we already have a value? display the form for deletion.
           case ActiveScaffold.js_framework
           when :jquery
@@ -12,22 +15,19 @@ module ActiveScaffold
             remove_file_js = "$(this).previous().value='true'; p=$(this).up(); p.hide(); p.next().show(); return false;";
           end            
           
-          hidden_options = options.dup
-          hidden_options[:id] += '_delete'
-          hidden_options[:name].sub!("[#{column.name}]", "[delete_#{column.name}]")
-          hidden_options[:value] = 'false'          
+          hidden_options = options.merge(:id => options[:id] + '_delete', :name => options[:name].sub("[#{column.name}]", "[delete_#{column.name}]"), :value => 'false')
           custom_hidden_field_tag = hidden_field(:record, column.name, hidden_options)
 
           content_tag(:div) do
             content_tag(:div) do
-              content = get_column_value(@record, column) + " #{custom_hidden_field_tag} | ".html_safe
+              content = get_column_value(record, column) + " #{custom_hidden_field_tag} | ".html_safe
               content += content_tag(:a, as_(:remove_file), {:href => '#', :onclick => remove_file_js})
               content += content_tag(:div, file_column_field("record", column.name, options), :style => "display: none")
             end           
           end
-       else
+        else
           file_column_field("record", column.name, options)          
-       end        
+        end        
       end
       
     end
