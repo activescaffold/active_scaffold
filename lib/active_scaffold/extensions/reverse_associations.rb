4b3fa9fea3de15db1ec4d02d1cd418d7cab2d3eb
Sergio Cambra <sergio@programatica.es>
2014-08-25 17:17:10 +0200

    fix reverse associations for rails master (4.2)

--- lib/active_scaffold/extensions/reverse_associations.rb
+++ lib/active_scaffold/extensions/reverse_associations.rb
@@ -1,12 +1,20 @@
-module ActiveRecord
-  module Reflection
-    class AssociationReflection #:nodoc:
+module ActiveScaffold
+  module ReverseAssociation
+    module CommonMethods
+      def self.included(base)
+        base.class_eval { attr_writer :reverse }
+        base.alias_method_chain :inverse_of, :autodetect
+      end
+
+      def inverse_of_with_autodetect
+        inverse_of_without_autodetect || autodetect_inverse
+      end
+
       def inverse_for?(klass)
         inverse_class = inverse_of.try(:active_record)
         inverse_class.present? && (inverse_class == klass || klass < inverse_class)
       end
 
-      attr_writer :reverse
       def reverse(klass = nil)
         unless defined? @reverse
           @reverse ||= inverse_of.try(:name)
@@ -14,56 +22,84 @@ module ActiveRecord
         @reverse || (autodetect_inverse(klass).try(:name) unless klass.nil?)
       end
 
-      def inverse_of_with_autodetect
-        inverse_of_without_autodetect || autodetect_inverse
+      def autodetect_inverse(klass = nil)
+        return nil if klass.nil? && options[:polymorphic]
+        klass ||= self.klass
+
+        # name-based matching (association name vs self.active_record.to_s)
+        matches = self.reverse_matches(klass)
+        if matches.length > 1
+          matches.find_all do |assoc|
+            self.active_record.to_s.underscore.include? assoc.name.to_s.pluralize.singularize
+          end
+        end
+
+        matches.first
       end
-      alias_method_chain :inverse_of, :autodetect
+    end
+
+    module AssociationReflection
+      def self.included(base)
+        base.send :include, ActiveScaffold::ReverseAssociation::CommonMethods
+      end
+      
+      protected
+      def reverse_matches(klass)
+        reverse_matches = []
+
+        # collect associations that point back to this model and use the same foreign_key
+        klass.reflect_on_all_associations.each do |assoc|
+          next if assoc == self
+          # skip over has_many :through associations
+          next if assoc.options[:through]
+          next unless assoc.options[:polymorphic] or assoc.class_name == self.active_record.name
+
+          case [assoc.macro, self.macro].find_all{|m| m == :has_and_belongs_to_many}.length
+            # if both are a habtm, then match them based on the join table
+            when 2
+            next unless assoc.options[:join_table] == self.options[:join_table]
+
+            # if only one is a habtm, they do not match
+            when 1
+            next
+
+            # otherwise, match them based on the foreign_key
+            when 0
+            next unless assoc.foreign_key.to_sym == self.foreign_key.to_sym
+          end
+
+          reverse_matches << assoc
+        end
+        reverse_matches
+      end
+    end
+
+    module ThroughReflection
+      def self.included(base)
+        base.send :include, ActiveScaffold::ReverseAssociation::CommonMethods
+      end
 
       protected
 
-        def autodetect_inverse(klass = nil)
-          return nil if klass.nil? && options[:polymorphic]
-          klass ||= self.klass
-          reverse_matches = []
+      def reverse_matches(klass)
+        reverse_matches = []
 
-          # stage 1 filter: collect associations that point back to this model and use the same foreign_key
-          klass.reflect_on_all_associations.each do |assoc|
-            next if assoc == self
-            if self.options[:through]
-              # only iterate has_many :through associations
-              next unless assoc.options[:through]
-              next unless assoc.through_reflection.klass == self.through_reflection.klass
-            else
-              # skip over has_many :through associations
-              next if assoc.options[:through]
-              next unless assoc.options[:polymorphic] or assoc.class_name == self.active_record.name
-
-              case [assoc.macro, self.macro].find_all{|m| m == :has_and_belongs_to_many}.length
-                # if both are a habtm, then match them based on the join table
-                when 2
-                next unless assoc.options[:join_table] == self.options[:join_table]
-
-                # if only one is a habtm, they do not match
-                when 1
-                next
-
-                # otherwise, match them based on the foreign_key
-                when 0
-                next unless assoc.foreign_key.to_sym == self.foreign_key.to_sym
-              end
-            end
-
-            reverse_matches << assoc
-          end
-
-          # stage 2 filter: name-based matching (association name vs self.active_record.to_s)
-          reverse_matches.find_all do |assoc|
-            self.active_record.to_s.underscore.include? assoc.name.to_s.pluralize.singularize
-          end if reverse_matches.length > 1
-
-          reverse_matches.first
+        # collect associations that point back to this model and use the same foreign_key
+        klass.reflect_on_all_associations.each do |assoc|
+          next if assoc == self
+          # only iterate has_many :through associations
+          next unless assoc.options[:through]
+          next unless assoc.class_name == self.active_record.name
+          next unless assoc.through_reflection.class_name == self.through_reflection.class_name
+          
+          reverse_matches << assoc
         end
-
+        reverse_matches
+      end
     end
+
   end
 end
+
+ActiveRecord::Reflection::AssociationReflection.send :include, ActiveScaffold::ReverseAssociation::AssociationReflection
+ActiveRecord::Reflection::ThroughReflection.send :include, ActiveScaffold::ReverseAssociation::ThroughReflection
--- test/mock_app/app/models/person.rb
+++ test/mock_app/app/models/person.rb
@@ -2,7 +2,7 @@ class Person < ActiveRecord::Base
   has_many :buildings, :foreign_key => :owner_id
   has_one :floor, :foreign_key => :tenant_id
   has_one :address, :through => :floor
-  has_one :home, :through => :floor, :source => :building
+  has_one :home, :through => :floor, :source => :building, :class_name => 'Building'
 
   has_many :contacts, :as => :contactable
   has_one :car, :dependent => :destroy
