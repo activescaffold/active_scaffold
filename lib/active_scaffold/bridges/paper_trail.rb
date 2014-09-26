84b946b336f14a4fd69f249b6c0929d055f8ae3e
Sergio Cambra <sergio@programatica.es>
2014-09-26 05:34:13 -1000

    add helper to hide action links on deleted list

--- lib/active_scaffold/bridges/paper_trail.rb
+++ lib/active_scaffold/bridges/paper_trail.rb
@@ -3,6 +3,7 @@ class ActiveScaffold::Bridges::PaperTrail < ActiveScaffold::DataStructures::Brid
     require File.join(File.dirname(__FILE__), "paper_trail/paper_trail_bridge")
     require File.join(File.dirname(__FILE__), "paper_trail/actions")
     require File.join(File.dirname(__FILE__), "paper_trail/config")
+    require File.join(File.dirname(__FILE__), "paper_trail/helper")
     ActiveScaffold::Config::Core.send :include, ActiveScaffold::Bridges::PaperTrail::PaperTrailBridge
   end
 end
