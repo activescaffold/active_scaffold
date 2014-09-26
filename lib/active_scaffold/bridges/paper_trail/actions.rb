d8ee205014b0427f8d6ee2ddbb761c6bb22a0a8f
Sergio Cambra <sergio@programatica.es>
2014-09-26 05:33:43 -1000

    fix paper trail bridge

--- lib/active_scaffold/bridges/paper_trail/actions.rb
+++ lib/active_scaffold/bridges/paper_trail/actions.rb
@@ -1,15 +1,17 @@
 module ActiveScaffold::Actions
   module DeletedRecords
     def self.included(base)
-      config = active_scaffold_config
-      if config.actions.include?(:nested) && config.deleted_records.nested_link_group
-        config.nested.add_link :versions, :label => config.deleted_records.nested_link_label, :action_group => config.deleted_records.nested_link_group
+      base.class_eval do
+        config = active_scaffold_config
+        if config.actions.include?(:nested) && config.deleted_records.nested_link_group
+          config.configure { nested.add_link :versions, :label => config.deleted_records.nested_link_label, :action_group => config.deleted_records.nested_link_group }
+        end
       end
     end
 
     def deleted
       query = PaperTrail::Version.destroys.where(:item_type => active_scaffold_config.model)
-      query = query.where_object(nested.child_association.foreign_key => nested.parent_id) if nested? && nested.child_association.macro == :belongs_to
+      query = query.where_object(nested.child_association.foreign_key => nested.parent_id) if nested? && nested.child_association.macro == :belongs_to && PaperTrail::Version.respond_to?(:where_object)
       pager = Paginator.new(query.count, active_scaffold_config.list.per_page) do |offset, per_page|
         query.offset(offset).limit(per_page).map(&:reify)
       end
