3a3cb153472521ade79430b52360ad2ac13f03df
Sergio Cambra <sergio@programatica.es>
2014-11-12 09:18:46 +0100

    fix routing on rails4, add test

---
+++ test/extensions/routing_mapper_test.rb
@@ -0,0 +1,21 @@
+require 'test_helper'
+
+class RoutingMapperTest < ActionController::TestCase
+  test 'rails routes' do
+    assert_routing 'addresses', :controller => 'addresses', :action => 'index'
+    assert_routing 'addresses/1', :controller => 'addresses', :action => 'show', :id => '1'
+    assert_routing 'addresses/1/edit', :controller => 'addresses', :action => 'edit', :id => '1'
+    assert_routing({:method => :put, :path => 'addresses/1'}, {:controller => 'addresses', :action => 'update', :id => '1'})
+    assert_routing({:method => :delete, :path => 'addresses/1'}, {:controller => 'addresses', :action => 'destroy', :id => '1'})
+    assert_routing({:method => :post, :path => 'addresses'}, {:controller => 'addresses', :action => 'create'})
+  end
+
+  test 'active scaffold routes' do
+    assert_routing 'addresses/show_search', :controller => 'addresses', :action => 'show_search'
+    assert_routing({:method => 'post', :path => 'addresses/render_field'}, :controller => 'addresses', :action => 'render_field')
+    assert_routing({:method => 'post', :path => 'addresses/2/render_field'}, :controller => 'addresses', :action => 'render_field', :id => '2')
+    assert_routing 'addresses/2/render_field', :controller => 'addresses', :action => 'render_field', :id => '2'
+    assert_routing 'addresses/edit_associated', :controller => 'addresses', :action => 'edit_associated'
+    assert_routing 'addresses/2/edit_associated', :controller => 'addresses', :action => 'edit_associated', :id => '2'
+  end
+end
--- test/mock_app/config/routes.rb
+++ test/mock_app/config/routes.rb
@@ -1,3 +1,6 @@
 RailsApp::Application.routes.draw do
+  resources :addresses do
+    as_routes
+  end
   match ':controller(/:action(/:id))', :via => :any
 end
