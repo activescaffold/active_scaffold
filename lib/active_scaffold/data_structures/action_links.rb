47539a03760011f68a148585dbbe2d008d4f0a09
Sergio Cambra <sergio@programatica.es>
2014-11-26 15:11:26 -1000

    default type is still :collection when adding on root group without :type option

--- lib/active_scaffold/data_structures/action_links.rb
+++ lib/active_scaffold/data_structures/action_links.rb
@@ -14,7 +14,7 @@ module ActiveScaffold::DataStructures
       link = if action.is_a?(ActiveScaffold::DataStructures::ActionLink) || action.is_a?(ActiveScaffold::DataStructures::ActionLinks)
         action
       else
-        options[:type] ||= default_type
+        options[:type] ||= default_type if default_type
         ActiveScaffold::DataStructures::ActionLink.new(action, options)
       end
       # NOTE: this duplicate check should be done by defining the comparison operator for an Action data structure
