5337755045d8ad0ce95d45ea92463bf3909784ac
Sergio Cambra <sergio@programatica.es>
2015-02-24 08:58:52 +0100

    clean tabs

--- test/misc/finder_test.rb
+++ test/misc/finder_test.rb
@@ -31,8 +31,8 @@ class FinderTest < MiniTest::Test
     tokens = %w(foo bar)
 
     expected_conditions = [
-    ['"model_stubs"."a" LIKE ? OR "model_stubs"."b" LIKE ?', '%foo%', '%foo%'],
-    ['"model_stubs"."a" LIKE ? OR "model_stubs"."b" LIKE ?', '%bar%', '%bar%']
+      ['"model_stubs"."a" LIKE ? OR "model_stubs"."b" LIKE ?', '%foo%', '%foo%'],
+      ['"model_stubs"."a" LIKE ? OR "model_stubs"."b" LIKE ?', '%bar%', '%bar%']
     ]
     assert_equal expected_conditions, ClassWithFinder.create_conditions_for_columns(tokens, columns)
 
