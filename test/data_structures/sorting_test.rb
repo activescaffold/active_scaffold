2c1824a14412ddf39b903162414e7b09c269bc35
Andrey Korobkov <korobkov@fryxell.info>
2015-02-24 23:57:31 +0300

    RuboCop: C: [Corrected] Use the new lambda literal syntax -> {...}.

--- test/data_structures/sorting_test.rb
+++ test/data_structures/sorting_test.rb
@@ -118,7 +118,7 @@ class SortingTest < MiniTest::Test
 
   def test_set_default_sorting_with_simple_default_scope
     model_stub_with_default_scope = ModelStub.clone
-    model_stub_with_default_scope.class_eval { default_scope lambda { order('a') } }
+    model_stub_with_default_scope.class_eval { default_scope -> { order('a') } }
     @sorting.set_default_sorting model_stub_with_default_scope
 
     assert @sorting.sorts_on?(:a)
@@ -128,7 +128,7 @@ class SortingTest < MiniTest::Test
 
   def test_set_default_sorting_with_complex_default_scope
     model_stub_with_default_scope = ModelStub.clone
-    model_stub_with_default_scope.class_eval { default_scope lambda { order('a DESC, players.last_name ASC') } }
+    model_stub_with_default_scope.class_eval { default_scope -> { order('a DESC, players.last_name ASC') } }
     @sorting.set_default_sorting model_stub_with_default_scope
 
     assert @sorting.sorts_on?(:a)
