11a1956f460d2794f2846f25a89bd429457efd3b
Sergio Cambra <sergio@programatica.es>
2015-02-27 12:44:34 +0100

    fix tests for rbx>=2.4 and rails 4.1

--- test/mock_app/config/environment.rb
+++ test/mock_app/config/environment.rb
@@ -2,4 +2,5 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
+RailsApp::Application.config.root = File.expand_path('../..', __FILE__)
 RailsApp::Application.initialize!
