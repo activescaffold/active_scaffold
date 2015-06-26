eaf9bc2f0fd96a8efdd4472a35721789544c31be
Sergio Cambra <sergio@programatica.es>
2015-06-26 14:31:56 +0200

    fix listening multiple events for chosen form_ui

--- app/assets/javascripts/jquery/active_scaffold_chosen.js
+++ app/assets/javascripts/jquery/active_scaffold_chosen.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function() {
-  jQuery(document).on('as:element_updated, as:element_created', function(event) {
+  jQuery(document).on('as:element_updated as:element_created', function(event) {
     jQuery('select.chosen', event.target).chosen();
   });
   jQuery(document).on('as:action_success', 'a.as_action', function(event, action_link) {
