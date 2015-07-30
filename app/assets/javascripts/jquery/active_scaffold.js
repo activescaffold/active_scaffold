ac1a660365040450c001401a50a79312a9538c74
Sergio Cambra <sergio@programatica.es>
2015-07-30 15:44:07 +0200

    autofocus first input if is inside .no-autofocus tag

--- app/assets/javascripts/jquery/active_scaffold.js
+++ app/assets/javascripts/jquery/active_scaffold.js
@@ -612,7 +612,9 @@ var ActiveScaffold = {
   focus_first_element_of_form: function(form_element, form_selector) {
     if (typeof(form_element) == 'string') form_element = '#' + form_element;
     if (typeof(form_selector) == 'undefined') form_selector = jQuery(form_element).is('form') ? '' : 'form ';
-    var input = jQuery(form_selector + ":input:visible:first", jQuery(form_element)).focus();
+    var input = jQuery(form_selector + ":input:visible:first", jQuery(form_element));
+    if (input.is('.no-autofocus :input')) return;
+    input.focus();
     try { if (input[0] && input[0].value) input[0].selectionStart = input[0].selectionEnd = input[0].value.length; } catch(e) {}
   },
 
