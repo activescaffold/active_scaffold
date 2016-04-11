36dc0b85a3a80a0579a9d74bc98708309e28f0ce
Sindre Wetjen <sindre.wetjen@gyldendal.no>
2016-04-11 13:38:40 +0200

    [Bridge] Make tinyMCE use the tinymce-rails configuration
    tinymce-rails specifies that one can use config/tinymce.yml[1] to
    configure layouts. Since ActiveScaffold relies on this gem, it would be
    fair to assume that one also would want to use the configuring methods
    used for the particular libary.
    
    This is done by collecting the configuration from the tinymce-rails
    libary itself, rejecting the selector, as active_scaffold will have to
    control that itself anyways. Also, introduce column option
    :tinymce_config to allow for switching between different layouts.
    
    [1]: https://github.com/spohlenz/tinymce-rails/blob/master/README.md

--- test/bridges/tiny_mce_test.rb
+++ test/bridges/tiny_mce_test.rb
@@ -4,6 +4,27 @@ class TinyMceTest < ActionView::TestCase
   include ActiveScaffold::Helpers::ViewHelpers
   include ActiveScaffold::Bridges::TinyMce::Helpers
 
+  # Mimic the behaviour of the tinymce-rails plugin function[1] to get
+  # configuration options from tinymce.yml
+  #
+  # [1]: https://github.com/spohlenz/tinymce-rails/blob/master/lib/tinymce/rails/helper.rb#L37
+  def tinymce_configuration(config=:default)
+    return case config
+           when :default
+             Class.new do
+               def options
+                 {theme: 'modern'}
+               end
+             end.new
+           when :alternate
+             Class.new do
+               def options
+                 {theme: 'alternate', toolbar: 'undo redo | format'}
+               end
+             end.new
+           end
+  end
+
   def test_includes
     ActiveScaffold::Bridges::TinyMce.expects(:install?).returns(true)
     ActiveScaffold.js_framework = :jquery
@@ -16,6 +37,15 @@ class TinyMceTest < ActionView::TestCase
     expects(:request).returns(stub(:xhr? => true))
 
     assert_dom_equal %{<textarea name=\"record[name]\" class=\"name-input mceEditor\" id=\"record_name\">\n</textarea>\n<script#{' type="text/javascript"' if Rails::VERSION::MAJOR < 4}>\n//<![CDATA[\ntinyMCE.settings = {\"theme\":\"modern\"};tinyMCE.execCommand('mceAddEditor', false, 'record_name');\n//]]>\n</script>}, active_scaffold_input_text_editor(config.columns[:name], :name => 'record[name]', :id => 'record_name', :class => 'name-input', :object => record)
+  end
+
+  def test_form_ui_alternate
+    config = ActiveScaffold::Config::Core.new(:company)
+    record = Company.new
+    expects(:request).returns(stub(:xhr? => true))
+    config.columns[:name].options[:tinymce_config] = :alternate
+
+    assert_dom_equal %{<textarea name=\"record[name]\" class=\"name-input mceEditor\" id=\"record_name\">\n</textarea>\n<script#{' type="text/javascript"' if Rails::VERSION::MAJOR < 4}>\n//<![CDATA[\ntinyMCE.settings = {\"theme\":\"alternate\",\"toolbar\":\"undo redo | format\"};tinyMCE.execCommand('mceAddEditor', false, 'record_name');\n//]]>\n</script>}, active_scaffold_input_text_editor(config.columns[:name], :name => 'record[name]', :id => 'record_name', :class => 'name-input', :object => record)
   end
 
   protected
