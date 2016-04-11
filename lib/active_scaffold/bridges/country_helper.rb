fe6b88223882ab16b06f79ff0a9a400c86fba2af
Sergio Cambra <sergio@programatica.es>
2016-04-11 19:41:12 +0200

    Remove own country_select helpers and convert into bridge for country_select gem
    CountrySelect provides more features, including few translations.
    A backward incompatible change may be found, CountrySelect saves country code
    instead of country name. To old way of country form_ui, set column options :format to :old

--- lib/active_scaffold/bridges/country_helper.rb
+++
@@ -1,9 +0,0 @@
-class ActiveScaffold::Bridges::CountryHelper < ActiveScaffold::DataStructures::Bridge
-  def self.install
-    require File.join(File.dirname(__FILE__), 'country_helper/country_helper_bridge.rb')
-  end
-
-  def self.install?
-    true
-  end
-end
