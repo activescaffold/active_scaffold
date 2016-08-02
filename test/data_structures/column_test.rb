6c340c5dd97312657ac6b4af2623ea10dc90e7a2
Andrey Korobkov <korobkov@fryxell.info>
2016-08-02 17:39:01 +0400

    fix some test

--- test/data_structures/column_test.rb
+++ test/data_structures/column_test.rb
@@ -80,7 +80,7 @@ class ColumnTest < MiniTest::Test
     # special comparisons
     refute @column.nil?
     assert @column != ''
-    assert @column.nonzero?
+    assert @column != 0
   end
 
   def test_ui
