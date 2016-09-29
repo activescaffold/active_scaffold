df172dab93b27fbd1bb6c80ed16fb399c294fe22
Sergio Cambra <sergio@programatica.es>
2016-09-29 13:30:48 +0200

    fix XSS via eid param, fixes #517

--- lib/active_scaffold/helpers/id_helpers.rb
+++ lib/active_scaffold/helpers/id_helpers.rb
@@ -3,7 +3,7 @@ module ActiveScaffold
     # A bunch of helper methods to produce the common view ids
     module IdHelpers
       def id_from_controller(controller)
-        controller.to_s.gsub('/', '__').html_safe
+        h controller.to_s.gsub('/', '__')
       end
 
       def controller_id(controller = (params[:eid] || nested_id || params[:parent_controller] || params[:controller]))
