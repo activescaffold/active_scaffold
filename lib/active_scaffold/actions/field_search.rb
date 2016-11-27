a2c3df59196d30455b07e0727ec18b6f6462960f
Andrey Korobkov <korobkov@fryxell.info>
2016-11-27 17:16:09 +0400

    Rubocop: fix Style/SymbolProc violation

--- lib/active_scaffold/actions/field_search.rb
+++ lib/active_scaffold/actions/field_search.rb
@@ -114,7 +114,7 @@ module ActiveScaffold::Actions
 
       def list_columns
         @list_columns ||= if grouped_search?
-                            columns = grouped_columns || super.select { |col| col.calculation? }
+                            columns = grouped_columns || super.select(&:calculation?)
                             [search_group_column || search_group_name].concat columns
         else
           super
