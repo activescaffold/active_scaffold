0114511d121e6cda6420b0fe78310750419a8fea
Sergio Cambra <sergio@programatica.es>
2017-08-11 16:38:17 +0200

    display grouped by when human_conditions is enabled

--- lib/active_scaffold/actions/field_search.rb
+++ lib/active_scaffold/actions/field_search.rb
@@ -160,7 +160,7 @@ module ActiveScaffold::Actions
             active_scaffold_conditions << search_condition
             filtered_columns << column
           end
-          if filtered_columns.present?
+          if filtered_columns.present? || grouped_search?
             @filtered = active_scaffold_config.field_search.human_conditions ? filtered_columns : true
           end
 
