0c038caeeeb9ca43a783914cf1097e91e99b1c79
Sergio Cambra <sergio@programatica.es>
2017-09-14 15:26:08 +0200

    remove onsubmit tag in forms and move tinymce related JS to assets

--- lib/active_scaffold/helpers/form_column_helpers.rb
+++ lib/active_scaffold/helpers/form_column_helpers.rb
@@ -534,8 +534,6 @@ module ActiveScaffold
         select_tag(options[:name], options_for_select(select_options, record.send(column.name)), options)
       end
 
-      def onsubmit; end
-
       ##
       ## Form column override signatures
       ##
