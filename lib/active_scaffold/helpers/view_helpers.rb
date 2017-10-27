activescaffold/active_scaffold

commit c0d6ac9b4b6bb40750ac902dd5169b81043783b3

Rubocop: fix Style/YodaCondition violation

lib/active_scaffold/helpers/view_helpers.rb
@@ -126,7 +126,7 @@ module ActiveScaffold
 
       def column_empty?(column_value)
         empty = column_value.nil?
-        empty ||= false != column_value && column_value.blank?
+        empty ||= column_value != false && column_value.blank?
         empty ||= ['&nbsp;', empty_field_text].include? column_value if column_value.is_a? String
         empty
       end

.rubocop_todo.yml
@@ -1,6 +1,6 @@
 # This configuration was generated by
 # `rubocop --auto-gen-config`
-# on 2017-09-25 19:32:36 +0400 using RuboCop version 0.50.0.
+# on 2017-09-25 19:37:12 +0400 using RuboCop version 0.50.0.
 # The point is for the user to remove these configuration records
 # one by one as the offenses are removed from the code base.
 # Note that changes in the inspected code, or installation of new
@@ -393,11 +393,3 @@ Style/TernaryParentheses:
     - 'lib/active_scaffold/data_structures/column.rb'
     - 'lib/active_scaffold/finder.rb'
     - 'lib/active_scaffold/helpers/search_column_helpers.rb'
-
-# Offense count: 1
-# Cop supports --auto-correct.
-# Configuration parameters: EnforcedStyle, SupportedStyles.
-# SupportedStyles: all_comparison_operators, equality_operators_only
-Style/YodaCondition:
-  Exclude:
-    - 'lib/active_scaffold/helpers/view_helpers.rb'

commit a7c70b770654fa988fe6c52b8f3387a20912e494

fix for IPAddr class for postgresql inet column type, broken by c0d6ac9, fixes #567

lib/active_scaffold/helpers/view_helpers.rb
@@ -126,7 +126,10 @@ module ActiveScaffold
 
       def column_empty?(column_value)
         empty = column_value.nil?
-        empty ||= column_value != false && column_value.blank?
+        # column_value != false would force boolean to be cast to integer
+        # when comparing to column_value of IPAddr class (PostgreSQL inet column type)
+        # rubocop:disable Style/YodaCondition
+        empty ||= false != column_value && column_value.blank?
         empty ||= ['&nbsp;', empty_field_text].include? column_value if column_value.is_a? String
         empty
       end