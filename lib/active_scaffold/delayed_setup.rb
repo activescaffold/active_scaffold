c3d2a8ab800f351a255b7c1c05b76b8a432ea855
Sergio Cambra <sergio@programatica.es>
2019-05-22 15:33:58 +0200

    fix delayed setup in threadsafe mode

--- lib/active_scaffold/delayed_setup.rb
+++ lib/active_scaffold/delayed_setup.rb
@@ -11,15 +11,20 @@ module ActiveScaffold
 
     module ClassMethods
       def active_scaffold(model_id = nil, &block)
+        @delayed_mutex ||= Mutex.new
         @active_scaffold_delayed = proc { super(model_id, &block) }
       end
 
       def config_active_scaffold_delayed
-        return unless @active_scaffold_delayed
-        @_prefixes = nil # clean prefixes in case is already cached, so our local_prefixes override is picked up
-        block = @active_scaffold_delayed
-        @active_scaffold_delayed = nil
-        block.call
+        return if @delayed_mutex.nil? || Thread.current["#{name}_running_delayed_init"]
+        @delayed_mutex.synchronize do
+          return unless @active_scaffold_delayed
+          @_prefixes = nil # clean prefixes in case is already cached, so our local_prefixes override is picked up
+          Thread.current["#{name}_running_delayed_init"] = true
+          block = @active_scaffold_delayed
+          @active_scaffold_delayed = nil # clear before called, active_scaffold_config may be called inside block
+          block.call.tap { Thread.current["#{name}_running_delayed_init"] = nil }
+        end.tap { @delayed_mutex = nil }
       end
 
       def active_scaffold_config
