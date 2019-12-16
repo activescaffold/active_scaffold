409a8f8e23f6dc4baa4c72c38decabba24a75621
Sergio Cambra <sergio@enpijama.es>
2019-12-16 13:49:38 +0100

    fix errors when config failed in one thread, avoid creating thread variables

--- lib/active_scaffold/delayed_setup.rb
+++ lib/active_scaffold/delayed_setup.rb
@@ -11,26 +11,27 @@ module ActiveScaffold
 
     module ClassMethods
       def active_scaffold(model_id = nil, &block)
-        @delayed_mutex ||= Mutex.new
-        @active_scaffold_delayed = proc { super(model_id, &block) }
+        @delayed_monitor ||= Monitor.new
+        @active_scaffold_delayed = proc do
+          begin
+            unless @active_scaffold_config # avoid running again if config is set (e.g. call active_scaffold_config inside block)
+              @_prefixes = nil # clean prefixes in case is already cached, so our local_prefixes override is picked up
+              super(model_id, &block)
+            end
+          rescue StandardError
+            # clear config variable if failed, so next request tries again
+            @active_scaffold_config = nil
+            raise
+          end
+        end
       end
 
       def config_active_scaffold_delayed
-        # Thread variable is used to disable this method while block is being eval'ed
-        return if @delayed_mutex.nil? || Thread.current["#{name}_running_delayed_init"]
-        @delayed_mutex.synchronize do
-          return unless @active_scaffold_delayed
-          @_prefixes = nil # clean prefixes in case is already cached, so our local_prefixes override is picked up
-          Thread.current["#{name}_running_delayed_init"] = true
-          begin
-            @active_scaffold_delayed.call
-            @active_scaffold_delayed = nil # not cleared if exception is raised, try again on next request
-          ensure
-            # ensure is cleared if exception is raised, to try again on next request
-            Thread.current["#{name}_running_delayed_init"] = nil
-          end
+        @delayed_monitor&.synchronize do
+          @active_scaffold_delayed&.call
+          @active_scaffold_delayed = nil if @active_scaffold_config # cleared only if config was set
         end
-        @delayed_mutex = nil # cleared only when config was loaded successfully
+        @delayed_monitor = nil if @active_scaffold_config # cleared only when config was loaded successfully
       end
 
       def active_scaffold_config
