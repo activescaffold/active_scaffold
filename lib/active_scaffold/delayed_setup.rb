570e4091e10c8e44fee8c3dcb581a5610cd67fdd
Sergio Cambra <sergio@enpijama.es>
2020-01-20 10:17:21 +0100

    clear active_scaffold_delayed proc and monitor inside active_scaffold_delayed proc
    So, when active_scaffold_delayed proc calls active_scaffold_config, it won't clear them

--- lib/active_scaffold/delayed_setup.rb
+++ lib/active_scaffold/delayed_setup.rb
@@ -14,10 +14,9 @@ module ActiveScaffold
         @delayed_monitor ||= Monitor.new
         @active_scaffold_delayed = proc do
           begin
-            unless @active_scaffold_config # avoid running again if config is set (e.g. call active_scaffold_config inside block)
-              @_prefixes = nil # clean prefixes in case is already cached, so our local_prefixes override is picked up
-              super(model_id, &block)
-            end
+            @_prefixes = nil # clean prefixes in case is already cached, so our local_prefixes override is picked up
+            super(model_id, &block)
+            @active_scaffold_delayed = @delayed_monitor = nil # after configuring, no need to keep proc or monitor
           rescue StandardError
             # clear config variable if failed, so next request tries again
             @active_scaffold_config = nil
@@ -28,10 +27,9 @@ module ActiveScaffold
 
       def config_active_scaffold_delayed
         @delayed_monitor&.synchronize do
-          @active_scaffold_delayed&.call
-          @active_scaffold_delayed = nil if @active_scaffold_config # cleared only if config was set
+          # if called in same thread while running config, do nothing
+          @active_scaffold_delayed&.call unless @active_scaffold_config
         end
-        @delayed_monitor = nil if @active_scaffold_config # cleared only when config was loaded successfully
       end
 
       def active_scaffold_config
