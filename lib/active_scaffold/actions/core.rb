73d790e6a6da9390904d7cdc3f2b377039897358
Sergio Cambra <sergio@enpijama.es>
2020-05-21 13:47:45 +0200

    support range conditions for numeric columns, parsing .. from param

--- lib/active_scaffold/actions/core.rb
+++ lib/active_scaffold/actions/core.rb
@@ -254,7 +254,7 @@ module ActiveScaffold::Actions
           next if active_scaffold_constraints[key]
           next if nested? && nested.param_name == key
 
-          range = %i[date datetime].include?(column.type) && value.is_a?(String) && value.scan('..').size == 1
+          range = %i[date datetime integer decimal float bigint].include?(column.type) && value.is_a?(String) && value.scan('..').size == 1
           value = value.split('..') if range
           value =
             if value.is_a?(Array)
