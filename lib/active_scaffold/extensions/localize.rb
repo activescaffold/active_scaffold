a9ccb70cb58197e20f4dc84deba3905cce6867e7
yisizawa <yisizawa@users.noreply.github.com>
2021-01-17 18:25:28 +0900

    fix i18n at ruby 3.0.0

--- lib/active_scaffold/extensions/localize.rb
+++ lib/active_scaffold/extensions/localize.rb
@@ -3,7 +3,7 @@ class Object
     if key.present?
       scope = [:active_scaffold, *options.delete(:scope)]
       options = options.reverse_merge(:scope => scope, :default => key.is_a?(String) ? key : key.to_s.titleize)
-      text = I18n.translate(key.to_s, options).html_safe # rubocop:disable Rails/OutputSafety
+      text = I18n.translate(key.to_s, **options).html_safe # rubocop:disable Rails/OutputSafety
       # text = nil if text.include?('translation missing:')
     end
     text || key
