fa854d498205004f2b2e34fdf755c9a5578aa080
Sergio Cambra <sergio@enpijama.es>
2021-03-12 13:40:29 +0100

    test render :super

--- test/mock_app/app/controllers/people_controller.rb
+++ test/mock_app/app/controllers/people_controller.rb
@@ -1,6 +1,8 @@
 class PeopleController < ApplicationController
   active_scaffold do |conf|
+    conf.columns.exclude :files
     conf.columns[:buildings].includes = nil
     conf.columns[:buildings].associated_limit = 0
+    conf.create.columns.exclude :address
   end
 end
