421d5d4179bc7ddb715c030401c58cdf2a60b66c
Sergio Cambra <sergio@enpijama.es>
2021-03-20 21:09:19 +0100

    add implicit_order_column to tableless for rails 6

--- lib/active_scaffold/tableless.rb
+++ lib/active_scaffold/tableless.rb
@@ -1,4 +1,8 @@
 class ActiveScaffold::Tableless < ActiveRecord::Base # rubocop:disable Rails/ApplicationRecord
+  if Rails.version > '6.0'
+    class_attribute :implicit_order_column, instance_accessor: false
+  end
+
   class AssociationScope < ActiveRecord::Associations::AssociationScope
     INSTANCE = create
     def self.scope(association, connection)
