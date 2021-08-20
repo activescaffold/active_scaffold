50089a7ce0546e5b1416c27f26db32ada1675303
dan <spatarel1@yahoo.com>
2021-08-20 03:18:55 +0300

    Defined the var chosen_options and instantiated chosen with its properties.

--- app/assets/javascripts/jquery/active_scaffold_chosen.js
+++ app/assets/javascripts/jquery/active_scaffold_chosen.js
@@ -1,14 +1,16 @@
+var chosen_options = {};
+
 jQuery(document).ready(function() {
   jQuery(document).on('as:element_updated as:element_created', function(event) {
-    jQuery('select.chosen', event.target).chosen();
+    jQuery('select.chosen', event.target).chosen(chosen_options);
   });
   jQuery(document).on('as:action_success', 'a.as_action', function(event, action_link) {
     if (action_link.adapter) {
-      jQuery('select.chosen', action_link.adapter).chosen();
+      jQuery('select.chosen', action_link.adapter).chosen(chosen_options);
     }
   });
-  jQuery('select.chosen').chosen();
+  jQuery('select.chosen').chosen(chosen_options);
   jQuery(document).on('turbolinks:load', function($) {
-      jQuery('select.chosen').chosen();
+      jQuery('select.chosen').chosen(chosen_options);
   });
 });
