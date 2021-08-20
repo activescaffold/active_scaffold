7f1510a6b47d4b99ffa95c5d0cd8d5e8fcda4713
dan <spatarel1@yahoo.com>
2021-08-20 04:20:41 +0300

    Included the jquery/tiny_mce_bridge_settings file.

--- lib/active_scaffold/bridges/tiny_mce.rb
+++ lib/active_scaffold/bridges/tiny_mce.rb
@@ -11,7 +11,7 @@ class ActiveScaffold::Bridges::TinyMce < ActiveScaffold::DataStructures::Bridge
   def self.javascripts
     case ActiveScaffold.js_framework
     when :jquery
-      ['tinymce-jquery', 'jquery/tiny_mce_bridge']
+      ['tinymce-jquery', 'jquery/tiny_mce_bridge', 'jquery/tiny_mce_bridge_settings']
     when :prototype
       ['tinymce', 'prototype/tiny_mce_bridge']
     end
