2aa14f67d57357d6c5dec22b445ac8a238c527b8
Sergio Cambra <sergio@enpijama.es>
2023-01-30 14:33:03 +0100

    Don't add includes or left joins for constrained columns, as they are ignored on sorting clause

--- lib/active_scaffold/actions/list.rb
+++ lib/active_scaffold/actions/list.rb
@@ -83,6 +83,7 @@ module ActiveScaffold::Actions
 
     def set_includes_for_sorting(columns, sorting)
       sorting.each_column do |col|
+        next if sorting.constraint_columns.include? col.name
         next unless col.includes.present? && !columns.include?(col)
         if active_scaffold_config.model.connection.needs_order_expressions_in_select?
           active_scaffold_references << col.includes
