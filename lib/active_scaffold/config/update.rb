da0636a6d5b98caf802afffeba88e2c9124c0acb
Sergio Cambra <sergio@enpijama.es>
2024-10-25 19:33:24 +0200

    improve support for optimistic locking
    hidden field for locking column is added automatically in base_form for update action, and any other action which has add_locking_column setting, or if partial is rendered passing add_locking_column in locals.
    
    on_stale_object_error is called in update action when StaleObjectError exception is rescued

--- lib/active_scaffold/config/update.rb
+++ lib/active_scaffold/config/update.rb
@@ -4,6 +4,7 @@ module ActiveScaffold::Config
     def initialize(core_config)
       super
       self.nested_links = self.class.nested_links
+      self.add_locking_column = self.class.add_locking_column
     end
 
     # global level configuration
@@ -21,6 +22,9 @@ module ActiveScaffold::Config
     cattr_accessor :nested_links, instance_accessor: false
     @@nested_links = false
 
+    cattr_accessor :add_locking_column, instance_accessor: false
+    @@add_locking_column = true
+
     columns_accessor :columns, :copy => :create
 
     # instance-level configuration
@@ -28,13 +32,15 @@ module ActiveScaffold::Config
 
     attr_accessor :nested_links
 
+    attr_accessor :add_locking_column
+
     attr_writer :hide_nested_column
     def hide_nested_column
       @hide_nested_column.nil? ? true : @hide_nested_column
     end
 
     UserSettings.class_eval do
-      user_attr :nested_links, :hide_nested_column
+      user_attr :nested_links, :hide_nested_column, :add_locking_column
     end
   end
 end
