0dc783c194897eea58589af45ad81861ec98c53a
Sergio Cambra <sergio@enpijama.es>
2024-12-03 20:39:04 +0100

    bump to 4.0.0.rc1

--- lib/active_scaffold/version.rb
+++ lib/active_scaffold/version.rb
@@ -1,9 +1,9 @@
 module ActiveScaffold
   module Version
-    MAJOR = 3
-    MINOR = 7
-    PATCH = 9
-    FIX = nil
+    MAJOR = 4
+    MINOR = 0
+    PATCH = 0
+    FIX = 'rc1'
 
     STRING = [MAJOR, MINOR, PATCH, FIX].compact.join('.')
   end
