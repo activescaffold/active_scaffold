4d46769d68ceee67e0c3805b335fe67bf5919f0b
Sergio Cambra <sergio@enpijama.es>
2025-03-11 22:47:59 +0100

    fix selected filter option when default option is set and option has been selected yet

--- lib/active_scaffold/helpers/action_link_helpers.rb
+++ lib/active_scaffold/helpers/action_link_helpers.rb
@@ -341,7 +341,11 @@ module ActiveScaffold
 
       def action_link_selected?(link, record)
         if link.respond_to?(:filter_name)
-          params[link.filter_name] == link.name.to_s
+          if params[link.filter_name]
+            params[link.filter_name] == link.name.to_s
+          else
+            active_scaffold_config.list.filters[link.filter_name].default_option == link.name
+          end
         else
           missing_options, url_options = replaced_action_link_url_options(link, record)
           safe_params = params.to_unsafe_h
