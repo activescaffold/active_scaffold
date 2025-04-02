addf987ca94b62299507024dced2c2e9eb152e23
Sergio Cambra <sergio@enpijama.es>
2025-04-02 19:06:45 +0200

    support tabbed_by in show action

--- lib/active_scaffold/helpers/show_column_helpers.rb
+++ lib/active_scaffold/helpers/show_column_helpers.rb
@@ -2,7 +2,7 @@ module ActiveScaffold
   module Helpers
     # Helpers that assist with the rendering of a List Column
     module ShowColumnHelpers
-      def show_column_value(record, column)
+      def show_column_value(record, column, **tab_options)
         value_record = column.delegated_association ? record.send(column.delegated_association.name) : record
         return get_column_value(record, column) unless value_record
 
@@ -14,7 +14,7 @@ module ActiveScaffold
           send(method, value_record, column)
         # second, check if the dev has specified a valid list_ui for this column
         elsif column.show_ui && (method = override_show_column_ui(column.show_ui))
-          send(method, value_record, column, ui_options: column.show_ui_options || column.options)
+          send(method, value_record, column, ui_options: tab_options.merge(column.show_ui_options || column.options))
         elsif column.column && (method = override_show_column_ui(column.column_type)) # rubocop:disable Lint/DuplicateBranch
           send(method, value_record, column)
         else
@@ -29,7 +29,8 @@ module ActiveScaffold
       def active_scaffold_show_horizontal(record, column, ui_options: column.options)
         raise ':horizontal show_ui must be used on association column' unless column.association
 
-        render partial: 'show_association', locals: {column: column, parent_record: record, show_partial: :horizontal}
+        render partial: 'show_association', locals: {column: column, parent_record: record, show_partial: :horizontal}.
+          merge(ui_options.slice(:tabbed_by, :tab_value, :tab_id))
       end
 
       def active_scaffold_show_vertical(record, column, ui_options: column.options)
