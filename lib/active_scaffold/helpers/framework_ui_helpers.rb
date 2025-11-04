e8863f1207e98da4d772c594e04b4ed70e3d1dfa
Sergio Cambra <sergio@enpijama.es>
2025-11-04 20:00:12 +0100

    fix calling as_ui_tag without content and block

--- lib/active_scaffold/helpers/framework_ui_helpers.rb
+++ lib/active_scaffold/helpers/framework_ui_helpers.rb
@@ -1,9 +1,10 @@
 module ActiveScaffold
   module Helpers
     module FrameworkUiHelpers
-      def as_ui_tag(key, content, options = {}, &block)
+      def as_ui_tag(key, content_or_options_with_block = nil, options = {}, &block)
+        options = content_or_options_with_block if block_given? && content_or_options_with_block.is_a?(Hash)
         tag, attributes = as_ui_for(key, options)
-        content_tag tag, content, attributes, &block
+        content_tag tag, content_or_options_with_block, attributes, &block
       end
 
       def as_ui_attributes(key, options = {})
