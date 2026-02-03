641dea83444fb7fbef4acfb33494015532cbef62
Sergio Cambra <sergio@enpijama.es>
2026-02-03 14:26:39 +0100

    fix rubocop offense

--- test/misc/finder_test.rb
+++ test/misc/finder_test.rb
@@ -33,7 +33,7 @@ class FinderTest < ActiveSupport::TestCase
 
   def test_method_sorting
     column = ActiveScaffold::DataStructures::Column.new('a', ModelStub)
-    column.sort_by method: proc { |r| self }
+    column.sort_by method: proc { self }
 
     collection = [16_000, 2853, 98_765, 6188, 4]
     assert_equal collection.sort, @klass.send(:sort_collection_by_column, collection, column, 'asc')
