73433638ce4d034578454954df4194a9f4adad17
Sergio Cambra <sergio@enpijama.es>
2026-02-27 12:57:19 +0100

    fix test, displaying label is handled in a different way

--- test/helpers/list_column_helpers_test.rb
+++ test/helpers/list_column_helpers_test.rb
@@ -18,16 +18,16 @@ class ListColumnHelpersTest < ActionView::TestCase
 
   def test_options_for_select_list_ui_for_simple_column
     @column.options[:options] = %i[value_1 value_2 value_3]
-    assert_equal 'Value 2', format_column_value(@record, @column)
+    assert_equal 'Value 2', get_column_value(@record, @column)
 
     @column.options[:options] = %w[value_1 value_2 value_3]
-    assert_equal 'value_2', format_column_value(@record, @column)
+    assert_equal 'value_2', get_column_value(@record, @column)
 
     @column.options[:options] = [%w[text_1 value_1], %w[text_2 value_2], %w[text_3 value_3]]
-    assert_equal 'text_2', format_column_value(@record, @column)
+    assert_equal 'text_2', get_column_value(@record, @column)
 
     @column.options[:options] = [%i[text_1 value_1], %i[text_2 value_2], %i[text_3 value_3]]
-    assert_equal 'Text 2', format_column_value(@record, @column)
+    assert_equal 'Text 2', get_column_value(@record, @column)
   end
 
   def test_association_join_text
